3704c4bbe6921e3e08c7e6811c460eb056fe1cc9
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-14 15:17:25 +0200

    Updated App Assets

--- lib/src/core/constants/app_assets.dart
+++ lib/src/core/constants/app_assets.dart
@@ -13,4 +13,23 @@ class AppAssets {
   static const appIconLogoBlack = 'assets/logo/icon-black.svg';
   static const appIconLogoWhite = 'assets/logo/icon-white.svg';
   static const appIconLogoYellow = 'assets/logo/icon-yellow.svg';
+
+  /* 
+  * =============== NavBar Icons =======================
+   */
+  static const favoriteIcon = 'assets/icons/navigation-bar/heart.svg';
+  static const homeIcon = 'assets/icons/navigation-bar/home.svg';
+  static const searchIcon = 'assets/icons/navigation-bar/search.svg';
+  static const profileIcon = 'assets/icons/navigation-bar/user.svg';
+
+  /* 
+  * =============== Social Media Icons =======================
+   */
+  static const facebookIcon = 'assets/icons/social-media-bar/facebook.svg';
+  static const linkedInIcon = 'assets/icons/social-media-bar/github.svg';
+  static const instagramIcon = 'assets/icons/social-media-bar/instagram.svg';
+  static const emailIcon = 'assets/icons/social-media-bar/linkedin.svg';
+  static const twitterIcon = 'assets/icons/social-media-bar/mail.svg';
+  static const youtubeIcon = 'assets/icons/social-media-bar/twitter.svg';
+  static const githubIcon = 'assets/icons/social-media-bar/youtube.svg';
 }
