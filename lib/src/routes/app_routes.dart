9a2944ffc029cbd052b1da24e108491e653b0c02
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-14 08:28:16 +0200

    Updated App Routes

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -1,3 +1,16 @@
 class AppRoutes {
+  static const introRoute = '/intro';
+  static const onBoardingRoute = '/onBoarding';
+  static const registrationRoute = '/registration';
   static const homeRoute = '/home';
+  static const cartRoute = '/cart';
+  static const categoriesRoute = '/categories';
+  static const checkoutRoute = '/checkout';
+  static const myInterestsRoute = '/myInterests';
+  static const orderConfirmationRoute = '/orderConfirmation';
+  static const productDetailsRoute = '/productDetails';
+  static const profileRoute = '/profile';
+  static const searchRoute = '/search';
+  static const wishlistRoute = '/wishlist';
+  static const deliveryRoute = '/delivery';
 }
