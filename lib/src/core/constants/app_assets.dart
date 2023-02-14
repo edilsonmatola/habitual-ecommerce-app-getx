ebe41cb61d60e81d92e4414f0e571d7a8ee84ab7
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-14 15:11:01 +0200

    Assets constant file

---
+++ lib/src/core/constants/app_assets.dart
@@ -0,0 +1,16 @@
+class AppAssets {
+  /* 
+  * =============== Logos =======================
+   */
+  static const appLogoPrimaryPng = 'assets/logo/logo-primary.png';
+  static const appLogoPrimary = 'assets/logo/logo-primary.svg';
+  static const appLogoBlack = 'assets/logo/logo-black.svg';
+  static const appLogoYellow = 'assets/logo/logo-yellow.svg';
+
+  /* 
+  * =============== Logos em forma de Icon =======================
+   */
+  static const appIconLogoBlack = 'assets/logo/icon-black.svg';
+  static const appIconLogoWhite = 'assets/logo/icon-white.svg';
+  static const appIconLogoYellow = 'assets/logo/icon-yellow.svg';
+}
