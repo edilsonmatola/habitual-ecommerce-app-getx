23c7cd3aba088a4e6a1696970b371b62bb4fe854
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-14 08:45:47 +0200

    Updated App Pages

---
+++ lib/src/routes/app_pages.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+import 'package:habitual/src/routes/app_routes.dart';
+
+// part 'app_routes.dart';
+
+class AppPages {
+  static const initial = AppRoutes.introRoute;
+
+  static final pages = <GetPage>[];
+}
