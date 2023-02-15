5abf66e5f891cb83216831282263cb9a372828ba
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-15 15:21:32 +0200

    Updated App Sizes

--- lib/src/core/constants/app_dimensions.dart
+++ lib/src/core/constants/app_dimensions.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-/// Constant AppDimensions to be used in the app (paddings, gaps, rounded corners etc.)
+/// Constant sizes to be used in the app (paddings, gaps, rounded corners etc.)
 class AppDimensions {
   static const p4 = 4.0;
   static const p8 = 8.0;
@@ -11,6 +11,8 @@ class AppDimensions {
   static const p32 = 32.0;
   static const p48 = 48.0;
   static const p64 = 64.0;
+  static const onboardingScreenPadding = EdgeInsets.fromLTRB(24, 48, 24, 55);
+  static const cartScreenPadding = EdgeInsets.fromLTRB(24, 48, 24, 55);
 }
 
 /// Constant gap widths
