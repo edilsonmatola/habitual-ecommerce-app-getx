5cacdaeeb16457573710d63fb0b2d1f352c6713a
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-15 15:20:44 +0200

    Updated App Assets

--- lib/src/core/constants/app_assets.dart
+++ lib/src/core/constants/app_assets.dart
@@ -32,4 +32,25 @@ class AppAssets {
   static const twitterIcon = 'assets/icons/social-media-bar/mail.svg';
   static const youtubeIcon = 'assets/icons/social-media-bar/twitter.svg';
   static const githubIcon = 'assets/icons/social-media-bar/youtube.svg';
+
+  /* 
+  * =============== Icons =======================
+   */
+  static const closeIcon = 'assets/icons/x.svg';
+  static const leftArrowIcon = 'assets/icons/arrow-left.svg';
+  static const circleRightArrowIcon = 'assets/icons/others/arrow-right-circle.svg';
+  static const circleLeftArrowIcon = 'assets/icons/others/arrow-left-circle.svg';
+  static const iOSLeftArrowIcon = 'assets/icons/others/chevron-left.svg';
+  static const iOSRightArrowIcon = 'assets/icons/others/chevron-right.svg';
+  static const eyeOffIcon = 'assets/icons/others/eye-off.svg';
+  static const eyeIcon = 'assets/icons/others/eye.svg';
+  static const lockIcon = 'assets/icons/others/lock.svg';
+  static const loginIcon = 'assets/icons/others/log-in.svg';
+  static const logoutIcon = 'assets/icons/others/log-out.svg';
+  static const shoppingBagIcon = 'assets/icons/others/shopping-bag.svg';
+  static const shoppingCartIcon = 'assets/icons/others/shopping-cart.svg';
+  static const filterIcon = 'assets/icons/others/sliders.svg';
+  static const deleteEmptyIcon = 'assets/icons/others/trash.svg';
+  static const deleteFullIcon = 'assets/icons/others/trash-2.svg';
+  static const lockOpenIcon = 'assets/icons/others/unlock.svg';
 }
---
+++ lib/src/core/constants/app_dimensions.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+/// Constant AppDimensions to be used in the app (paddings, gaps, rounded corners etc.)
+class AppDimensions {
+  static const p4 = 4.0;
+  static const p8 = 8.0;
+  static const p12 = 12.0;
+  static const p16 = 16.0;
+  static const p20 = 20.0;
+  static const p24 = 24.0;
+  static const p32 = 32.0;
+  static const p48 = 48.0;
+  static const p64 = 64.0;
+}
+
+/// Constant gap widths
+const gapW4 = SizedBox(width: AppDimensions.p4);
+const gapW8 = SizedBox(width: AppDimensions.p8);
+const gapW12 = SizedBox(width: AppDimensions.p12);
+const gapW16 = SizedBox(width: AppDimensions.p16);
+const gapW20 = SizedBox(width: AppDimensions.p20);
+const gapW24 = SizedBox(width: AppDimensions.p24);
+const gapW32 = SizedBox(width: AppDimensions.p32);
+const gapW48 = SizedBox(width: AppDimensions.p48);
+const gapW64 = SizedBox(width: AppDimensions.p64);
+
+/// Constant gap heights
+const gapH4 = SizedBox(height: AppDimensions.p4);
+const gapH8 = SizedBox(height: AppDimensions.p8);
+const gapH12 = SizedBox(height: AppDimensions.p12);
+const gapH16 = SizedBox(height: AppDimensions.p16);
+const gapH20 = SizedBox(height: AppDimensions.p20);
+const gapH24 = SizedBox(height: AppDimensions.p24);
+const gapH32 = SizedBox(height: AppDimensions.p32);
+const gapH48 = SizedBox(height: AppDimensions.p48);
+const gapH64 = SizedBox(height: AppDimensions.p64);
