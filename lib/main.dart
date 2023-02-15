524796163d2f00b67821a95572cd1cab0ec687f8
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-15 12:32:25 +0200

    Code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+import 'src/core/core_export.dart';
+import 'src/routes/routes_export.dart';
+import 'src/theme/app_themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +17,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: AppThemes().lightTheme,
+      darkTheme: AppThemes().darkTheme,
+      title: AppTitles.appTitle,
+      initialRoute: AppRoutes.introRoute,
+      getPages: AppPages.pages,
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget!),
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(
+            350,
+            name: MOBILE,
+          ),
+          const ResponsiveBreakpoint.autoScale(
+            600,
+            name: TABLET,
+            scaleFactor: 1.3,
+          ),
+          const ResponsiveBreakpoint.autoScale(
+            800,
+            name: DESKTOP,
+          ),
+          const ResponsiveBreakpoint.autoScale(
+            1200,
+            name: 'XL',
+            scaleFactor: 1.4,
+          ),
+        ],
       ),
     );
   }
