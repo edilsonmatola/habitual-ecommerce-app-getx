0b8b30a4f250fed17455bc68a8447d58cffa28f0
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-15 07:58:42 +0200

    App Titles

---
+++ lib/src/core/constants/app_titles.dart
@@ -0,0 +1,42 @@
+class AppTitles {
+  static const introTitle = "Welcome to the world's largest you store.";
+  static const appTitle = 'Habitual';
+  static const whishlistTitle = 'Wishlist';
+  static const cartTitle = 'Cart';
+  static const checkoutTitle = 'Checkout';
+  static const categoriesTitle = 'Categories';
+  static const profileTitle = 'Profile';
+  static const orderTitle = 'Orders';
+
+  /* 
+  * ========== Onboarding Titles ================================
+   */
+  static const onboarding1Title = 'Irrelevant results again?';
+  static const onboarding1Description =
+      'Habitual sends you relevant items based off of your habits and interests.';
+  static const onboarding2Title = 'Curated products by our professionals.';
+  static const onboarding2Description =
+      'Tell us what you like. No, really, it helps a bunch when we serve you some great products. You just keep doing your thing.';
+  static const onboarding3Title = 'Free shipping and concierge services. ';
+  static const onboarding3Description =
+      "No fees, free shipping and amazing customer service. We'll get you your package within 2 business days no questions asked!";
+
+  /* 
+  * ========== Registration Titles ================================
+   */
+  static const registrationStep1Title = 'Add a photo.';
+  static const registrationStep1Description =
+      'Add a photo so other members know who you are.';
+  static const registrationStep2Title =
+      'What do you want to achieve with Habitual?';
+  static const registrationStep2Description =
+      'This will help us make a unique experience that is just for you.';
+  static const registrationStep3Title =
+      'Get started by picking some interests.';
+  static const registrationStep4Title = "Let's dig deeper!";
+  static const registrationStep4Description =
+      'We have some recommended options for the interests that you have chosen.';
+  static const registrationCompleteTitle = 'Woohoo!';
+  static const registrationCompleteDescription =
+      'Registration complete! Get ready to have the best shopping experiences of your life.';
+}
