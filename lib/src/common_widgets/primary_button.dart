991f55d38972a57620151d1aa6637e41616732f1
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-15 21:37:02 +0200

    Updated Button Color

--- lib/src/common_widgets/primary_button.dart
+++ lib/src/common_widgets/primary_button.dart
@@ -5,21 +5,27 @@ class PrimaryButton extends StatelessWidget {
     Key? key,
     this.onPressed,
     required this.buttonLabel,
+    this.color,
   }) : super(key: key);
 
   final VoidCallback? onPressed;
   final String buttonLabel;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
+        backgroundColor: color,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(6),
         ),
       ),
       onPressed: onPressed,
-      child: Text(buttonLabel),
+      child: Text(
+        buttonLabel,
+        style: Theme.of(context).textTheme.labelMedium,
+      ),
     );
   }
 }
