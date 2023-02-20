f0b3e28b71470f77d2ad705abe6fab9a794ea7ff
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-20 22:53:46 +0200

    Added onbording screens

--- lib/src/routes/app_pages.dart
+++ lib/src/routes/app_pages.dart
@@ -3,6 +3,9 @@
 import 'package:get/get.dart';
 
 import '../presentation/introduction_screen/introduction_screen.dart';
+import '../presentation/onboarding_screen/onboarding1_screen.dart';
+import '../presentation/onboarding_screen/onboarding2_screen.dart';
+import '../presentation/onboarding_screen/onboarding3_screen.dart';
 import 'app_routes.dart';
 
 // part 'app_routes.dart';
@@ -14,6 +17,21 @@ class AppPages {
     GetPage(
       name: AppRoutes.introRoute,
       page: () => const IntroductionScreen(),
+    ),
+    /* 
+    * ===== Onboarding Pages =====
+     */
+    GetPage(
+      name: AppRoutes.onBoarding1Route,
+      page: () => const OnBoarding1Screen(),
+    ),
+    GetPage(
+      name: AppRoutes.onBoarding2Route,
+      page: () => const OnBoarding2Screen(),
+    ),
+    GetPage(
+      name: AppRoutes.onBoarding3Route,
+      page: () => const OnBoarding3Screen(),
     ),
   ];
 }
