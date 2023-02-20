76be62d7188673f51ea3ade0d882abfa0f3440e5
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-20 22:54:22 +0200

    Added onBoarding screeens routes

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -1,7 +1,13 @@
 class AppRoutes {
   static const introRoute = '/intro';
-  static const onBoardingRoute = '/onBoarding';
-  static const registrationRoute = '/registration';
+  static const onBoarding1Route = '/onBoarding1';
+  static const onBoarding2Route = '/onBoarding2';
+  static const onBoarding3Route = '/onBoarding3';
+  static const registration1Route = '/registration1';
+  static const registration2Route = '/registration2';
+  static const registration3Route = '/registration3';
+  static const registration4Route = '/registration4';
+  static const registrationCompleteRoute = '/registrationComplete';
   static const homeRoute = '/home';
   static const cartRoute = '/cart';
   static const categoriesRoute = '/categories';
