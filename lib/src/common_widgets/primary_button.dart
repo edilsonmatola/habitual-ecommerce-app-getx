b7aa06a1c1e42fb318a3cd47bf87f8668bf8dd8d
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-20 22:55:21 +0200

    Updated button properties

--- lib/src/common_widgets/primary_button.dart
+++ lib/src/common_widgets/primary_button.dart
@@ -6,25 +6,32 @@ class PrimaryButton extends StatelessWidget {
     this.onPressed,
     required this.buttonLabel,
     this.color,
+    this.buttonWidth = 342,
+    this.buttonHeight = 48,
   }) : super(key: key);
 
   final VoidCallback? onPressed;
   final String buttonLabel;
   final Color? color;
+  final double? buttonWidth, buttonHeight;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: color,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(6),
+    return SizedBox(
+      width: buttonWidth,
+      height: buttonHeight,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6),
+          ),
         ),
-      ),
-      onPressed: onPressed,
-      child: Text(
-        buttonLabel,
-        style: Theme.of(context).textTheme.labelMedium,
+        onPressed: onPressed,
+        child: Text(
+          buttonLabel,
+          style: Theme.of(context).textTheme.labelMedium,
+        ),
       ),
     );
   }
