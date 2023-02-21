d677ced3161c697eb982863ffcb19883282671be
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-21 10:22:53 +0200

    App Typography Style

---
+++ lib/src/theme/app_style.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+
+import '../core/core_export.dart';
+
+// ignore: avoid_classes_with_only_static_members
+class AppStyle {
+  // * ========== HEADINGS ================
+  // * Heading 1 (H1)
+  static final heading1Style = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.loraFontFamily,
+    fontSize: 40,
+    fontWeight: AppFonts.loraSemiBold,
+  );
+  // * Heading 2 (H2)
+  static final heading2Style = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.loraFontFamily,
+    fontSize: 32,
+    fontWeight: AppFonts.loraSemiBold,
+  );
+  // * Heading 3 (H3)
+  static final heading3Style = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.loraFontFamily,
+    fontSize: 24,
+    fontWeight: AppFonts.loraSemiBold,
+  );
+  // * Heading 4 (H4)
+  static final heading4Style = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.loraFontFamily,
+    fontSize: 18,
+    fontWeight: AppFonts.loraSemiBold,
+  );
+  // * Heading 5 (H5)
+  static final heading5Style = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.loraFontFamily,
+    fontSize: 14,
+    fontWeight: AppFonts.loraSemiBold,
+  );
+
+// * ========= BODY (Parapgraphs) ===========
+// * p1-b (Paragraph 1 Bold)
+  static final paragraph1Bold = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 16,
+    fontWeight: AppFonts.interMedium,
+  );
+// * p1-r (Paragraph 1 Regular)
+  static final paragraph1Regular = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 16,
+    fontWeight: AppFonts.interRegular,
+  );
+// * p2-b (Paragraph 2 Bold)
+  static final paragraph2Bold = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 14,
+    fontWeight: AppFonts.interMedium,
+  );
+// * p2-r (Paragraph 2 Regular)
+  static final paragraph2Regular = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 14,
+    fontWeight: AppFonts.interMedium,
+  );
+// * p3-b (Paragraph 3 Bold)
+  static final paragraph3Bold = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 12,
+    fontWeight: AppFonts.interMedium,
+  );
+
+// * p3-r (Paragraph 3 Regular)
+  static final paragraph3Regular = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 12,
+    fontWeight: AppFonts.interRegular,
+  );
+
+// * ========= Labels ===========
+// * l1-b (Label 1 Bold)
+  static final label1Bold = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 20,
+    fontWeight: AppFonts.interMedium,
+  );
+// * l1-r (Label 1 Regular)
+  static final label1Regular = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 20,
+    fontWeight: AppFonts.interRegular,
+  );
+// * l2-b (Label 2 Bold)
+  static final label2Bold = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 16,
+    fontWeight: AppFonts.interMedium,
+  );
+// * l2-r (Label 2 Regular)
+  static final label2Regular = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 16,
+    fontWeight: AppFonts.interRegular,
+  );
+// * l3-b (Label 3 Bold)
+  static final label3Bold = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 14,
+    fontWeight: AppFonts.interMedium,
+  );
+// * l3-r (Label 3 Regular)
+  static final label3Regular = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 14,
+    fontWeight: AppFonts.interRegular,
+  );
+// * l4-b (Label 4 Bold)
+  static final label4Bold = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 12,
+    fontWeight: AppFonts.interMedium,
+  );
+// * l4-b (Label 4 Bold)
+  static final label4Regular = TextStyle(
+    fontStyle: FontStyle.normal,
+    overflow: TextOverflow.ellipsis,
+    color: AppColors.neutral800,
+    fontFamily: AppFonts.interFontFamily,
+    fontSize: 12,
+    fontWeight: AppFonts.interRegular,
+  );
+}
