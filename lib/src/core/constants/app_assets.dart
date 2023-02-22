1f541786997b82d4aa6723047e624395bcc2c0c4
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-22 09:34:33 +0200

    Moved Icons to app_icons

--- lib/src/core/constants/app_assets.dart
+++ lib/src/core/constants/app_assets.dart
@@ -1,6 +1,7 @@
+///* Imagery Styles
 class AppAssets {
   /* 
-  * =============== Logos =======================
+  * ============= Logos ==============
    */
   static const appLogoPrimaryPng = 'assets/logo/logo-primary.png';
   static const appLogoPrimary = 'assets/logo/logo-primary.svg';
@@ -8,30 +9,11 @@ class AppAssets {
   static const appLogoYellow = 'assets/logo/logo-yellow.svg';
 
   /* 
-  * =============== Logos em forma de Icon =======================
+  * ========== Logos em forma de Icon =========
    */
-  static const appIconLogoBlack = 'assets/logo/icon-black.svg';
-  static const appIconLogoWhite = 'assets/logo/icon-white.svg';
-  static const appIconLogoYellow = 'assets/logo/icon-yellow.svg';
-
-  /* 
-  * =============== NavBar Icons =======================
-   */
-  static const favoriteIcon = 'assets/icons/navigation-bar/heart.svg';
-  static const homeIcon = 'assets/icons/navigation-bar/home.svg';
-  static const searchIcon = 'assets/icons/navigation-bar/search.svg';
-  static const profileIcon = 'assets/icons/navigation-bar/user.svg';
-
-  /* 
-  * =============== Social Media Icons =======================
-   */
-  static const facebookIcon = 'assets/icons/social-media-bar/facebook.svg';
-  static const linkedInIcon = 'assets/icons/social-media-bar/github.svg';
-  static const instagramIcon = 'assets/icons/social-media-bar/instagram.svg';
-  static const emailIcon = 'assets/icons/social-media-bar/linkedin.svg';
-  static const twitterIcon = 'assets/icons/social-media-bar/mail.svg';
-  static const youtubeIcon = 'assets/icons/social-media-bar/twitter.svg';
-  static const githubIcon = 'assets/icons/social-media-bar/youtube.svg';
+  static const appLogoBlackSmall = 'assets/logo/icon-black.svg';
+  static const appLogoWhiteSmall = 'assets/logo/icon-white.svg';
+  static const appLogoYellowSmall = 'assets/logo/icon-yellow.svg';
 
   /* 
   * ==== Paralax Background Images ====
@@ -48,27 +30,4 @@ class AppAssets {
       'assets/images/intro_screen/paralax-background/image-5.svg';
   static const paralaxImage6 =
       'assets/images/intro_screen/paralax-background/image-6.svg';
-
-  /* 
-  * =============== Icons =======================
-   */
-  static const closeIcon = 'assets/icons/x.svg';
-  static const leftArrowIcon = 'assets/icons/arrow-left.svg';
-  static const circleRightArrowIcon =
-      'assets/icons/others/arrow-right-circle.svg';
-  static const circleLeftArrowIcon =
-      'assets/icons/others/arrow-left-circle.svg';
-  static const iOSLeftArrowIcon = 'assets/icons/others/chevron-left.svg';
-  static const iOSRightArrowIcon = 'assets/icons/others/chevron-right.svg';
-  static const eyeOffIcon = 'assets/icons/others/eye-off.svg';
-  static const eyeIcon = 'assets/icons/others/eye.svg';
-  static const lockIcon = 'assets/icons/others/lock.svg';
-  static const loginIcon = 'assets/icons/others/log-in.svg';
-  static const logoutIcon = 'assets/icons/others/log-out.svg';
-  static const shoppingBagIcon = 'assets/icons/others/shopping-bag.svg';
-  static const shoppingCartIcon = 'assets/icons/others/shopping-cart.svg';
-  static const filterIcon = 'assets/icons/others/sliders.svg';
-  static const deleteEmptyIcon = 'assets/icons/others/trash.svg';
-  static const deleteFullIcon = 'assets/icons/others/trash-2.svg';
-  static const lockOpenIcon = 'assets/icons/others/unlock.svg';
 }
