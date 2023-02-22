cd46aca1263b2d5fb1c8801603753356f7eb4422
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-22 08:05:36 +0200

    Flutter Test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,82 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:ecommerce_app/models/product.dart';
+// import 'package:ecommerce_app/screens/cart_screen.dart';
+// import 'package:ecommerce_app/screens/product_detail_screen.dart';
+// import 'package:ecommerce_app/screens/products_screen.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+// void main() {
+//   group('Ecommerce App Tests', () {
+//     testWidgets('Product Detail Screen Test', (WidgetTester tester) async {
+//       // Define a product
+//       final product = Product(
+//         id: 'p1',
+//         title: 'Product 1',
+//         description: 'Product 1 description',
+//         price: 9.99,
+//         imageUrl: 'https://image.com/product1.jpg',
+//       );
 
-import 'package:habitual/main.dart';
+//       // Build the ProductDetailScreen widget with the product
+//       await tester.pumpWidget(MaterialApp(home: ProductDetailScreen(product)));
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+//       // Verify that the product details are displayed correctly
+//       expect(find.text(product.title), findsOneWidget);
+//       expect(find.text(product.description), findsOneWidget);
+//       expect(
+//           find.text('\$${product.price.toStringAsFixed(2)}'), findsOneWidget);
+//     });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+//     testWidgets('Add to Cart Test', (WidgetTester tester) async {
+//       // Define a product
+//       final product = Product(
+//         id: 'p1',
+//         title: 'Product 1',
+//         description: 'Product 1 description',
+//         price: 9.99,
+//         imageUrl: 'https://image.com/product1.jpg',
+//       );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+//       // Build the ProductsScreen widget with the product
+//       await tester.pumpWidget(MaterialApp(home: ProductsScreen()));
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
-}
+//       // Tap the product item to open the ProductDetailScreen
+//       await tester.tap(find.text(product.title));
+//       await tester.pumpAndSettle();
+
+//       // Tap the "Add to Cart" button
+//       await tester.tap(find.byKey(Key('add-to-cart')));
+//       await tester.pumpAndSettle();
+
+//       // Verify that the product is added to the cart
+//       expect(find.text('1'), findsOneWidget);
+//     });
+
+//     testWidgets('Remove from Cart Test', (WidgetTester tester) async {
+//       // Define a product
+//       final product = Product(
+//         id: 'p1',
+//         title: 'Product 1',
+//         description: 'Product 1 description',
+//         price: 9.99,
+//         imageUrl: 'https://image.com/product1.jpg',
+//       );
+
+//       // Build the CartScreen widget with the product
+//       await tester.pumpWidget(MaterialApp(home: CartScreen()));
+
+//       // Add the product to the cart
+//       await tester.tap(find.text('Add to Cart'));
+//       await tester.pumpAndSettle();
+
+//       // Verify that the product is added to the cart
+//       expect(find.text('1'), findsOneWidget);
+
+//       // Remove the product from the cart
+//       await tester.tap(find.byKey(Key('remove-from-cart')));
+//       await tester.pumpAndSettle();
+
+//       // Verify that the product is removed from the cart
+//       expect(find.text('0'), findsOneWidget);
+//     });
+//   });
+// }
