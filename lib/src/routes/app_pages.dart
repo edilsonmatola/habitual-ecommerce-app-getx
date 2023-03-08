c62e42304d68e88fd754011f6821d9cfb86142dd
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-08 14:49:11 +0200

    Code Refactor

--- lib/src/routes/app_pages.dart
+++ lib/src/routes/app_pages.dart
@@ -2,15 +2,15 @@
 
 import 'package:get/get.dart';
 
+import '../presentation/home_screen/home_tab_screen.dart';
 import '../presentation/introduction_screen/introduction_screen.dart';
 import '../presentation/onboarding_screen/onboarding1_screen.dart';
 import '../presentation/onboarding_screen/onboarding2_screen.dart';
 import '../presentation/onboarding_screen/onboarding3_screen.dart';
-import 'app_routes.dart';
 
 // part 'app_routes.dart';
 
-class AppPages {
+abstract class AppPages {
   static const initial = AppRoutes.introRoute;
 
   static final pages = <GetPage>[
@@ -33,5 +33,32 @@ class AppPages {
       name: AppRoutes.onBoarding3Route,
       page: () => const OnBoarding3Screen(),
     ),
+    GetPage(
+      name: AppRoutes.homeRoute,
+      page: () => const HomeTabScreen(),
+    ),
   ];
+}
+
+abstract class AppRoutes {
+  static const introRoute = '/intro';
+  static const onBoarding1Route = '/onBoarding1';
+  static const onBoarding2Route = '/onBoarding2';
+  static const onBoarding3Route = '/onBoarding3';
+  static const registration1Route = '/registration1';
+  static const registration2Route = '/registration2';
+  static const registration3Route = '/registration3';
+  static const registration4Route = '/registration4';
+  static const registrationCompleteRoute = '/registrationComplete';
+  static const homeRoute = '/home';
+  static const cartRoute = '/cart';
+  static const categoriesRoute = '/categories';
+  static const checkoutRoute = '/checkout';
+  static const myInterestsRoute = '/myInterests';
+  static const orderConfirmationRoute = '/orderConfirmation';
+  static const productDetailsRoute = '/productDetails';
+  static const profileRoute = '/profile';
+  static const searchRoute = '/search';
+  static const wishlistRoute = '/wishlist';
+  static const deliveryRoute = '/delivery';
 }
--- lib/src/routes/routes_export.dart
+++ lib/src/routes/routes_export.dart
@@ -1,2 +1,1 @@
 export 'app_pages.dart';
-export 'app_routes.dart';
