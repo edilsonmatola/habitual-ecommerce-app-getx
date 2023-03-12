08a633dc770842bba25582d116269f6b634d0529
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-12 12:58:55 +0200

    Bottom NavBar

---
+++ lib/src/presentation/base_screen/widgets/custom_bottom_navbar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../core/constants/app_colors.dart';
+import '../../../core/constants/app_icons.dart';
+
+class CustomBottomNavBar extends StatefulWidget {
+  const CustomBottomNavBar({
+    Key? key,
+    required this.screenController,
+  }) : super(key: key);
+
+  final PageController screenController;
+
+  @override
+  State<CustomBottomNavBar> createState() => _CustomBottomNavBarState();
+}
+
+class _CustomBottomNavBarState extends State<CustomBottomNavBar> {
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      onTap: (index) {
+        setState(() {
+          currentPage = index;
+          // widget.screenController.jumpToPage(index);
+          widget.screenController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.ease,
+          );
+        });
+      },
+      currentIndex: currentPage,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: AppColors.white,
+      items: [
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            AppIcons.homeIcon,
+            color:
+                currentPage == 0 ? AppColors.neutral900 : AppColors.neutral400,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            AppIcons.favoriteIcon,
+            color:
+                currentPage == 1 ? AppColors.neutral900 : AppColors.neutral400,
+          ),
+          label: 'Wishlist',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            AppIcons.profileIcon,
+            color:
+                currentPage == 2 ? AppColors.neutral900 : AppColors.neutral400,
+          ),
+          label: 'Profile',
+        ),
+        BottomNavigationBarItem(
+          icon: SvgPicture.asset(
+            AppIcons.searchIcon,
+            color:
+                currentPage == 3 ? AppColors.neutral900 : AppColors.neutral400,
+          ),
+          label: 'Search',
+        ),
+      ],
+    );
+  }
+}
