0d8b6ea07d8468c4f6510e3ab0f043a5871bc448
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-12 11:48:58 +0200

    Wishlist Screen

---
+++ lib/src/presentation/wishlist_screen/wishlist_tab_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WishlistTabScreen extends StatefulWidget {
+  const WishlistTabScreen({super.key});
+
+  @override
+  State<WishlistTabScreen> createState() => _WishlistTabScreenState();
+}
+
+class _WishlistTabScreenState extends State<WishlistTabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
