d4c21a923bda76b2696cce0e44063fc1e917037a
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-12 12:23:23 +0200

    Search Screen

---
+++ lib/src/presentation/search_screen/search_tab_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchTabScreen extends StatefulWidget {
+  const SearchTabScreen({super.key});
+
+  @override
+  State<SearchTabScreen> createState() => _SearchTabScreenState();
+}
+
+class _SearchTabScreenState extends State<SearchTabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
