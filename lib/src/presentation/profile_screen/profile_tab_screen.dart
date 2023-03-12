68e67b8793539b6803d3ad850e06ea9fdf90b914
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-12 12:22:58 +0200

    Profile screen

---
+++ lib/src/presentation/profile_screen/profile_tab_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileTabScreen extends StatefulWidget {
+  const ProfileTabScreen({super.key});
+
+  @override
+  State<ProfileTabScreen> createState() => _ProfileTabScreenState();
+}
+
+class _ProfileTabScreenState extends State<ProfileTabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
