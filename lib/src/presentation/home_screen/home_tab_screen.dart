cad7ee6f5a2bb168729f04653b448ddf1f1c3538
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-23 22:50:47 +0200

    Displaying product cards

--- lib/src/presentation/home_screen/home_tab_screen.dart
+++ lib/src/presentation/home_screen/home_tab_screen.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 
 import '../../core/core_export.dart';
 import 'widgets/custom_app_bar.dart';
-import 'widgets/product_card.dart';
+import 'widgets/main_card.dart';
 
 class HomeTabScreen extends StatefulWidget {
   const HomeTabScreen({super.key});
@@ -17,12 +17,9 @@ class _HomeTabScreenState extends State<HomeTabScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // * App Bar
+            // * AppBar
             Padding(
               padding: EdgeInsets.symmetric(
                 horizontal: AppDimensions.p24,
@@ -36,81 +33,51 @@ class _HomeTabScreenState extends State<HomeTabScreen> {
             gapH32,
             // * Products Cards
             Padding(
-              padding: const EdgeInsets.only(
-                left: AppDimensions.p24,
+              padding: const EdgeInsets.symmetric(
+                horizontal: AppDimensions.p24,
               ),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
                     'Trending',
                     style: Theme.of(context).textTheme.headlineSmall,
                   ),
+                  Text('> <')
                 ],
               ),
             ),
             gapH16,
             SizedBox(
-              height: Get.size.height * .50,
+              height: Get.size.height * .45,
               child: ListView.separated(
                 scrollDirection: Axis.horizontal,
                 padding: EdgeInsets.symmetric(horizontal: AppDimensions.p24),
                 physics: const BouncingScrollPhysics(),
-                shrinkWrap: true,
                 itemCount: 10,
-                separatorBuilder: (context, index) => SizedBox(
-                  width: AppDimensions.p16,
-                ),
-                itemBuilder: (context, index) => ProductCard(),
+                separatorBuilder: (context, index) => gapW16,
+                itemBuilder: (context, index) => MainCard(),
               ),
             ),
             gapH32,
-            Padding(
-              padding: const EdgeInsets.only(
-                left: AppDimensions.p24,
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    'Deals',
-                    style: Theme.of(context).textTheme.headlineSmall,
+            SizedBox(
+              height: Get.size.height * .30,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                physics: const BouncingScrollPhysics(),
+                padding: EdgeInsets.symmetric(horizontal: AppDimensions.p24),
+                itemCount: 10,
+                separatorBuilder: (context, index) => gapW16,
+                itemBuilder: (context, index) => SizedBox(
+                  width: 50,
+                  height: 50,
+                  child: Card(
+                    color: AppColors.red300,
                   ),
-                ],
-              ),
-            ),
-            gapH16,
-            SizedBox(
-              height: Get.size.height * .30,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                padding: EdgeInsets.symmetric(horizontal: AppDimensions.p24),
-                physics: const BouncingScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: 10,
-                separatorBuilder: (context, index) => SizedBox(
-                  width: AppDimensions.p16,
-                ),
-                itemBuilder: (context, index) => ProductCard(
-                  width: 163,
-                  height: 80,
-                  color: AppColors.neutral100,
                 ),
               ),
             ),
             gapH32,
-            SizedBox(
-              height: Get.size.height * .30,
-              child: GridView(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                ),
-                children: [
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
-                  ProductCard(),
-                ],
-              ),
-            ),
           ],
         ),
       ),
