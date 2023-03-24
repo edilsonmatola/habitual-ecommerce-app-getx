51ed97e743df642a3614ea4392a4842549a2e4c3
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-24 12:45:19 +0200

    Rounded Icon Button

--- lib/src/common_widgets/outlined_rounded_icon_button.dart
+++ lib/src/common_widgets/outlined_rounded_icon_button.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:habitual/src/core/constants/constants_export.dart';
 
 class OutlinedRoundedIconButton extends StatelessWidget {
   const OutlinedRoundedIconButton({
     Key? key,
     this.onPressed,
-    required this.buttonLabel, required this.icon,
+    required this.icon,
   }) : super(key: key);
 
   final VoidCallback? onPressed;
-  final String buttonLabel;
   final String icon;
 
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton.icon(
-      onPressed: onPressed,
-      label: Text(''),
-      icon: SvgPicture.asset(icon),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        padding: const EdgeInsets.all(AppDimensions.p10),
+        width: 36,
+        height: 36,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 1,
+            color: AppColors.neutral800,
+          ),
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: Ink(
+          child: SvgPicture.asset(
+            icon,
+            width: AppDimensions.p16,
+            height: AppDimensions.p16,
+            color: AppColors.neutral800,
+          ),
+        ),
+      ),
     );
   }
 }
