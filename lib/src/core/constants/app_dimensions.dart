ee011501f10d980a6f5b73094067e40a333eb914
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-24 12:43:58 +0200

    Added more paddings

--- lib/src/core/constants/app_dimensions.dart
+++ lib/src/core/constants/app_dimensions.dart
@@ -5,6 +5,7 @@ class AppDimensions {
   static const p2 = 2.0;
   static const p4 = 4.0;
   static const p8 = 8.0;
+  static const p10 = 10.0;
   static const p12 = 12.0;
   static const p16 = 16.0;
   static const p20 = 20.0;
@@ -14,6 +15,7 @@ class AppDimensions {
   static const p56 = 56.0;
   static const p64 = 64.0;
   static const p70 = 70.0;
+  static const p100 = 100.0;
   static const p280 = 280.0;
 }
 
@@ -29,6 +31,7 @@ const cartScreenPadding = EdgeInsets.fromLTRB(
 const gapW2 = SizedBox(width: AppDimensions.p2);
 const gapW4 = SizedBox(width: AppDimensions.p4);
 const gapW8 = SizedBox(width: AppDimensions.p8);
+const gapW10 = SizedBox(width: AppDimensions.p10);
 const gapW12 = SizedBox(width: AppDimensions.p12);
 const gapW16 = SizedBox(width: AppDimensions.p16);
 const gapW20 = SizedBox(width: AppDimensions.p20);
@@ -38,12 +41,14 @@ const gapW48 = SizedBox(width: AppDimensions.p48);
 const gapW56 = SizedBox(width: AppDimensions.p56);
 const gapW64 = SizedBox(width: AppDimensions.p64);
 const gapW70 = SizedBox(width: AppDimensions.p70);
+const gapW100 = SizedBox(width: AppDimensions.p100);
 const gapW280 = SizedBox(height: AppDimensions.p280);
 
 /// Constant gap heights
 const gapH2 = SizedBox(height: AppDimensions.p2);
 const gapH4 = SizedBox(height: AppDimensions.p4);
 const gapH8 = SizedBox(height: AppDimensions.p8);
+const gapH10 = SizedBox(height: AppDimensions.p10);
 const gapH12 = SizedBox(height: AppDimensions.p12);
 const gapH16 = SizedBox(height: AppDimensions.p16);
 const gapH20 = SizedBox(height: AppDimensions.p20);
@@ -53,4 +58,5 @@ const gapH48 = SizedBox(height: AppDimensions.p48);
 const gapH56 = SizedBox(height: AppDimensions.p56);
 const gapH64 = SizedBox(height: AppDimensions.p64);
 const gapH70 = SizedBox(height: AppDimensions.p70);
+const gapH100 = SizedBox(height: AppDimensions.p100);
 const gapH280 = SizedBox(height: AppDimensions.p280);
