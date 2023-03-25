e1c9e9a41d2e5eb67e9c9543db10cfd60c25d0e8
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-25 07:43:57 +0200

    AppSizes

--- lib/src/core/constants/constants_export.dart
+++ lib/src/core/constants/constants_export.dart
@@ -1,6 +1,6 @@
 export 'app_assets.dart';
 export 'app_colors.dart';
-export 'app_dimensions.dart';
+export 'app_sizes.dart';
 export 'app_fonts.dart';
 export 'app_icons.dart';
 export 'app_titles.dart';
