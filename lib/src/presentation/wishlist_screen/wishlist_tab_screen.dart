6095d1711be5feca271a43add520926965121e91
Edilson Matola <edilsonmatola13@gmail.com>
2023-03-29 22:03:16 +0200

    Empty wishlist card

--- lib/src/presentation/wishlist_screen/wishlist_tab_screen.dart
+++ lib/src/presentation/wishlist_screen/wishlist_tab_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:habitual/src/core/constants/constants_export.dart';
 
 class WishlistTabScreen extends StatefulWidget {
   const WishlistTabScreen({super.key});
@@ -8,8 +10,31 @@ class WishlistTabScreen extends StatefulWidget {
 }
 
 class _WishlistTabScreenState extends State<WishlistTabScreen> {
+  final deviceWidth = Get.size.width;
+  final deviceHeight = Get.size.height;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        // padding: ,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(
+                AppSizes.p32,
+              ),
+              width: 341,
+              height: 394,
+              decoration: BoxDecoration(
+                color: AppColors.purple300,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
