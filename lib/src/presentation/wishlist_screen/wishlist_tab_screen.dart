69e4400ab9473eb688d0dfb6f4c2083cc4904d28
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-05 23:54:34 +0200

    Empty card

--- lib/src/presentation/wishlist_screen/wishlist_tab_screen.dart
+++ lib/src/presentation/wishlist_screen/wishlist_tab_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:habitual/src/core/constants/constants_export.dart';
+
+import '../../common_widgets/common_widgets_export.dart';
 
 class WishlistTabScreen extends StatefulWidget {
   const WishlistTabScreen({super.key});
@@ -30,6 +33,56 @@ class _WishlistTabScreenState extends State<WishlistTabScreen> {
               height: 394,
               decoration: BoxDecoration(
                 color: AppColors.purple300,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: AppColors.purple300,
+                borderRadius: BorderRadius.circular(10),
+                // boxShadow: AppShadows.cardShadowLarge,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    SvgPicture.asset(
+                      'assets/images/wishlist_screen/products-empty.svg',
+                      height: 64,
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    const Text(
+                      "You have no saved products",
+                      // style: AppTextStyles.h4.copyWith(
+                      //   fontWeight: FontWeight.w800,
+                      //   color: AppColors.textGray_80,
+                      // ),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    const Text(
+                      "You have no saved products. Start saving to add to wishlists or create one.",
+                      textAlign: TextAlign.center,
+                      // style: AppTextStyles.bodyRegular.copyWith(
+                      //   fontWeight: FontWeight.w400,
+                      //   color: AppColors.textGray_60,
+                      // ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    PrimaryButton(
+                      buttonLabel: "Create a wishlist",
+                      onPressed: () {},
+                      // iconLeft: Icons.add,
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
