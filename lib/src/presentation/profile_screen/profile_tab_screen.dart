1da9f1172bfa302b79fedc631e53a4f211961f84
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-07 18:23:13 +0200

    Building up profile screen

--- lib/src/presentation/profile_screen/profile_tab_screen.dart
+++ lib/src/presentation/profile_screen/profile_tab_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../core/core_export.dart';
+
 class ProfileTabScreen extends StatefulWidget {
   const ProfileTabScreen({super.key});
 
@@ -10,6 +12,19 @@ class ProfileTabScreen extends StatefulWidget {
 class _ProfileTabScreenState extends State<ProfileTabScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.only(
+          left: AppSizes.p24,
+          right: AppSizes.p24,
+          top: AppSizes.p56,
+          bottom: AppSizes.p32,
+        ),
+        child: Column(
+          children:  [],
+        ),
+      ),
+    );
   }
 }
