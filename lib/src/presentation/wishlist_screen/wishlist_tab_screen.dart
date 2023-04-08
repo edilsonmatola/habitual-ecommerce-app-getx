62fa6224d735d0a012904327cf90cf4107c49fa1
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-08 22:49:16 +0200

    Code refactor

--- lib/src/presentation/wishlist_screen/wishlist_tab_screen.dart
+++ lib/src/presentation/wishlist_screen/wishlist_tab_screen.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 import 'package:habitual/src/core/constants/constants_export.dart';
 
 import '../../common_widgets/common_widgets_export.dart';
-import '../home_screen/widgets/custom_app_bar.dart';
 import '../home_screen/widgets/main_card.dart';
 
 class WishlistTabScreen extends StatefulWidget {
@@ -20,96 +19,117 @@ class _WishlistTabScreenState extends State<WishlistTabScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.only(
-          left: AppSizes.p16,
-          right: AppSizes.p16,
-          top: AppSizes.p56,
-          bottom: AppSizes.p32,
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            CustomAppBar(
-              onPressed: () {},
-              iconButton: AppIcons.shoppingCartIcon,
-            ),
-            gapH32,
-            Container(
-              width: deviceWidth * .50,
-              padding: const EdgeInsets.all(
-                AppSizes.p32,
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              centerTitle: false,
+              title: Text(
+                AppTitles.whishlistTitle,
+                style: Get.textTheme.headlineSmall,
               ),
-              decoration: BoxDecoration(
-                color: AppColors.purple300,
-                borderRadius: BorderRadius.circular(AppSizes.p10),
-                // boxShadow: AppShadows.cardShadowLarge,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(AppSizes.p16),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 24,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 24,
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      AppIcons.shoppingCartIcon,
                     ),
-                    SvgPicture.asset(
-                      'assets/images/wishlist_screen/products-empty.svg',
-                      width: 145,
-                      height: 145,
-                    ),
-                    gapH24,
-                    Text(
-                      'Uh Oh! You have no saved products.',
-                      style: Get.textTheme.headlineSmall,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    PrimaryButton(
-                      buttonColor: AppColors.white,
-                      labelColor: AppColors.neutral800,
-                      buttonLabel: '+ Create a wishlist',
-                      onPressed: () {},
-                      // iconLeft: Icons.add,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            gapH32,
-            Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Text(
-                    'Trending',
-                    style: Theme.of(context).textTheme.headlineSmall,
                   ),
                 ),
-                PrimaryTextButton(
-                  buttonLabel: 'View all',
-                  onPressed: () {},
-                )
               ],
             ),
-            gapH16,
-            SizedBox(
-              height: Get.size.height * .45,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                // padding: const EdgeInsets.symmetric(
-                //   horizontal: AppSizes.p16,
-                // ),
-                physics: const BouncingScrollPhysics(),
-                itemCount: 10,
-                separatorBuilder: (context, index) => gapW16,
-                itemBuilder: (context, index) => const MainCard(),
+          ];
+        },
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          padding: const EdgeInsets.symmetric(
+            horizontal: AppSizes.p16,
+            vertical: AppSizes.p32,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: deviceWidth * .50,
+                padding: const EdgeInsets.all(
+                  AppSizes.p32,
+                ),
+                decoration: BoxDecoration(
+                  color: AppColors.purple300,
+                  borderRadius: BorderRadius.circular(
+                    AppSizes.p10,
+                  ),
+                  // boxShadow: AppShadows.cardShadowLarge,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(AppSizes.p16),
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      SvgPicture.asset(
+                        'assets/images/wishlist_screen/products-empty.svg',
+                        width: 145,
+                        height: 145,
+                      ),
+                      gapH24,
+                      Text(
+                        'Uh Oh! You have no saved products.',
+                        style: Get.textTheme.headlineSmall,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      PrimaryButton(
+                        buttonColor: AppColors.white,
+                        labelColor: AppColors.neutral800,
+                        buttonLabel: '+ Create a wishlist',
+                        onPressed: () {},
+                        // iconLeft: Icons.add,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
+              gapH32,
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'Trending',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ),
+                  PrimaryTextButton(
+                    buttonLabel: 'View all',
+                    onPressed: () {},
+                  )
+                ],
+              ),
+              gapH16,
+              SizedBox(
+                height: Get.size.height * .45,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  // padding: const EdgeInsets.symmetric(
+                  //   horizontal: AppSizes.p16,
+                  // ),
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: 10,
+                  separatorBuilder: (context, index) => gapW16,
+                  itemBuilder: (context, index) => const MainCard(),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
