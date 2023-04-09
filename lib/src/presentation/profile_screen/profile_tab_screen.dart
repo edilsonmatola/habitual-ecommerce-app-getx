a7ca5cb537519c8bfffec82ab22ef4d4e767dd74
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-09 15:49:30 +0200

    Profile Bio

--- lib/src/presentation/profile_screen/profile_tab_screen.dart
+++ lib/src/presentation/profile_screen/profile_tab_screen.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
+import '../../common_widgets/common_widgets_export.dart';
 import '../../core/core_export.dart';
 
 class ProfileTabScreen extends StatefulWidget {
@@ -14,6 +16,8 @@ class ProfileTabScreen extends StatefulWidget {
 class _ProfileTabScreenState extends State<ProfileTabScreen> {
   @override
   Widget build(BuildContext context) {
+    final deviceWidth = Get.size.width;
+    final deviceHeight = Get.size.height;
     return Scaffold(
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
@@ -39,12 +43,123 @@ class _ProfileTabScreenState extends State<ProfileTabScreen> {
           ),
         ],
         body: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(
-            horizontal: AppSizes.p24,
-            vertical: AppSizes.p32,
+          padding: const EdgeInsets.only(
+            left: AppSizes.p24,
+            right: AppSizes.p24,
+            bottom: AppSizes.p32,
           ),
           child: Column(
-            children: const [],
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Leslie Flores',
+                          style: Get.textTheme.displayLarge,
+                        ),
+                        gapH8,
+                        Text(
+                          'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia.',
+                          style: Get.textTheme.titleLarge?.copyWith(
+                            fontWeight: AppFonts.interRegular,
+                          ),
+                        ),
+                        gapH8,
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: PrimaryOutlinedButton(
+                            isText: true,
+                            title: 'Edit',
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Expanded(
+                    child: Center(
+                      child: SizedBox(
+                        width: 73,
+                        height: 73,
+                        child: CircleAvatar(
+                          radius: 100,
+                          backgroundImage: CachedNetworkImageProvider(
+                            'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=761&q=80',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              gapH24,
+              Container(
+                width: deviceWidth * .50,
+                padding: const EdgeInsets.all(
+                  AppSizes.p32,
+                ),
+                decoration: BoxDecoration(
+                  color: AppColors.red300,
+                  borderRadius: BorderRadius.circular(
+                    AppSizes.p10,
+                  ),
+                  // boxShadow: AppShadows.cardShadowLarge,
+                ),
+                child: Column(
+                  children: [
+                    SvgPicture.asset(
+                      'assets/images/profile_screen/profile-empty.svg',
+                      width: 145,
+                      height: 145,
+                    ),
+                    gapH24,
+                    Text(
+                      'What interests you!',
+                      style: Get.textTheme.headlineSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                    gapH8,
+                    Text(
+                      "You don't have any interests listed. Tell us what you love the most and we'll recommend relevant products to you.",
+                      style: Get.textTheme.titleLarge?.copyWith(
+                        color: AppColors.neutral700,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    gapH24,
+                    PrimaryButton(
+                      buttonColor: AppColors.white,
+                      labelColor: AppColors.neutral800,
+                      buttonLabel: '+ Add my interests',
+                      onPressed: () {},
+                      // iconLeft: Icons.add,
+                    ),
+                    gapH24,
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'Account',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    ),
+                  ),
+                  PrimaryTextButton(
+                    buttonLabel: 'View all',
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              gapH16,
+            ],
           ),
         ),
       ),
