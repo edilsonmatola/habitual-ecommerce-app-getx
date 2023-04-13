8e8e33b17a6a112979b92a923678f17a5ddd7012
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-13 22:33:27 +0200

    Localization Folder

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class L10n {
+  static final all = [
+    const Locale('en'),
+    const Locale('hi'),
+    const Locale('es'),
+    const Locale('de'),
+  ];
+}
