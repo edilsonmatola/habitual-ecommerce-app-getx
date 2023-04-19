3de938b6629ab4188e1c01f0254aaef3c2cd94e4
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-19 07:20:00 +0200

    Search Bar widget

---
+++ lib/src/presentation/search_screen/widgets/search_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:habitual/src/core/constants/app_colors.dart';
+import 'package:habitual/src/core/constants/app_fonts.dart';
+import 'package:habitual/src/core/constants/app_icons.dart';
+import 'package:habitual/src/core/constants/app_sizes.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        prefixIcon: Padding(
+          padding: const EdgeInsets.all(
+            AppSizes.p20,
+          ),
+          child: SvgPicture.asset(
+            AppIcons.searchIcon,
+          ),
+        ),
+        hintText: 'Search by keyword or categories',
+        hintStyle: Get.textTheme.displaySmall?.copyWith(
+          color: AppColors.neutral400,
+          fontWeight: AppFonts.interRegular,
+        ),
+        isDense: true,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            width: 0,
+            style: BorderStyle.none,
+          ),
+        ),
+      ),
+    );
+  }
+}
