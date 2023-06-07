14b05c970b433b0573bab4613c6a6b9fe02d96fb
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-07 13:33:59 +0200

    Removed const from Page

--- lib/src/routes/app_pages.dart
+++ lib/src/routes/app_pages.dart
@@ -54,7 +54,7 @@ abstract class AppPages {
     ),
     GetPage(
       name: AppRoutes.productDetailsRoute,
-      page: () => const ProductDetailsScreen(),
+      page: () => ProductDetailsScreen(),
       transitionDuration: const Duration(milliseconds: 300),
       curve: Curves.easeOut,
       transition: Transition.downToUp,
