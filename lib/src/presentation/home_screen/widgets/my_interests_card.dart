0333f3a896011a5717c83e8db27d2fbedc3e7343
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-09 09:22:06 +0200

    Code refactor

--- lib/src/presentation/home_screen/widgets/my_interests_card.dart
+++ lib/src/presentation/home_screen/widgets/my_interests_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:habitual/src/core/core_export.dart';
@@ -46,7 +47,7 @@ class _MyInterestsCardState extends State<MyInterestsCard>
   Widget build(BuildContext context) {
     return SizedBox(
       width: Sizes.deviceWidth * .90,
-      height: Sizes.deviceHeight * .65,
+      height: Sizes.deviceHeight * .70,
       child: Card(
         color: AppColors.yellow100,
         child: Padding(
@@ -89,24 +90,78 @@ class _MyInterestsCardState extends State<MyInterestsCard>
                 child: TabBarView(
                   controller: _tabController,
                   children: [
-                    Column(
-                      children: [
-                        Container(
-                          width: Sizes.deviceWidth,
-                          height: Sizes.deviceHeight * .12,
-                          color: AppColors.blue500,
-                        ),
-                        Container(
-                          width: Sizes.deviceWidth,
-                          height: Sizes.deviceHeight * .12,
-                          color: AppColors.yellow500,
-                        ),
-                        Container(
-                          width: Sizes.deviceWidth,
-                          height: Sizes.deviceHeight * .12,
-                          color: AppColors.red500,
-                        ),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: Sizes.p16,
+                      ),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: Sizes.deviceWidth,
+                            height: Sizes.deviceHeight * .12,
+                            child: InkWell(
+                              child: Row(
+                                children: [
+                                  CachedNetworkImage(
+                                    imageUrl:
+                                        'https://media.croma.com/image/upload/v1685969095/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256605_li76nl.png',
+                                    width: 80,
+                                    height: 80,
+                                  ),
+                                  gapW16,
+                                  Row(
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            'Macbook Pro 13"',
+                                            style: Get.textTheme.displayMedium,
+                                          ),
+                                          Text(
+                                            'Apple',
+                                            style: Get.textTheme.titleMedium
+                                                ?.copyWith(
+                                              color: AppColors.neutral700,
+                                              fontWeight: Fonts.interRegular,
+                                            ),
+                                          ),
+                                          gapH4,
+                                          Text(
+                                            '\$1934.99',
+                                            style: Get.textTheme.bodyMedium,
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                              onTap: () {},
+                            ),
+                          ),
+                          gapH8,
+                          Container(
+                            width: Sizes.deviceWidth,
+                            height: Sizes.deviceHeight * .12,
+                            color: AppColors.yellow500,
+                          ),
+                          gapH8,
+                          Container(
+                            width: Sizes.deviceWidth,
+                            height: Sizes.deviceHeight * .12,
+                            color: AppColors.red500,
+                          ),
+                          gapH16,
+                          PrimaryButton(
+                            buttonLabel: 'View "Tech" products',
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
                     ),
                     const Center(
                       child: Text(
