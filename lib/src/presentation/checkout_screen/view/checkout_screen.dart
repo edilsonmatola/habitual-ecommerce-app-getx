a403b169cd9ddb14dbf62c6e81fb47a55d85ac72
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-13 15:58:16 +0200

    Add Navigation

--- lib/src/presentation/checkout_screen/view/checkout_screen.dart
+++ lib/src/presentation/checkout_screen/view/checkout_screen.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:habitual/src/common_widgets/common_widgets_export.dart';
 import 'package:habitual/src/common_widgets/custom_divider.dart';
 import 'package:habitual/src/presentation/checkout_screen/view/components/promo_code_textfield.dart';
+import 'package:habitual/src/routes/app_pages.dart';
 
 import '../../../core/core_export.dart';
 
@@ -23,134 +24,139 @@ class CheckoutScreen extends StatelessWidget {
               ),
             ),
           ],
-          body: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(
-              horizontal: Sizes.p24,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Payment Method',
-                      style: Get.textTheme.displayLarge,
-                    ),
-                    PrimaryOutlinedButton(
-                      hasText: true,
-                      title: 'Edit',
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-                gapH16,
-                //* Bank Card
-                Container(
-                  width: 342,
-                  height: 192,
-                  color: AppColors.blue500,
-                ),
-                gapH24,
-                const CustomDivider(),
-                gapH24,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Address',
-                      style: Get.textTheme.displayLarge,
-                    ),
-                    PrimaryOutlinedButton(
-                      hasText: true,
-                      title: 'Edit',
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-                gapH16,
-                Text(
-                  'Leslie Flores',
-                  style: Get.textTheme.bodyLarge?.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                gapH8,
-                Text(
-                  '''2972 Westheimer Rd. 
-Santa Ana, Illinois 
-85486, United States of America''',
-                  style: Get.textTheme.titleLarge?.copyWith(
-                    color: AppColors.neutral600,
-                  ),
-                ),
-                gapH24,
-                const CustomDivider(),
-                gapH24,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Expanded(
-                      flex: 3,
-                      child: PromoCodeTextField(),
-                    ),
-                    gapW8,
-                    Expanded(
-                      child: PrimaryButton(
-                        buttonHeight: 60,
-                        buttonColor: AppColors.neutral800,
-                        buttonLabel: 'Apply',
+          body: ScrollConfiguration(
+            behavior: const ScrollBehavior().copyWith(overscroll: false),
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.p24,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Payment Method',
+                        style: Get.textTheme.displayLarge,
+                      ),
+                      PrimaryOutlinedButton(
+                        hasText: true,
+                        title: 'Edit',
                         onPressed: () {},
                       ),
-                    ),
-                  ],
-                ),
-                gapH24,
-                const CustomDivider(),
-                gapH24,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Delivery',
-                      style: Get.textTheme.displayLarge?.copyWith(
-                        fontWeight: FontWeight.bold,
+                    ],
+                  ),
+                  gapH16,
+                  //* Bank Card
+                  Container(
+                    width: 342,
+                    height: 192,
+                    color: AppColors.blue500,
+                  ),
+                  gapH24,
+                  const CustomDivider(),
+                  gapH24,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Address',
+                        style: Get.textTheme.displayLarge,
                       ),
-                    ),
-                    Text(
-                      'Free',
-                      style: Get.textTheme.titleLarge?.copyWith(
-                        fontWeight: Fonts.interRegular,
+                      PrimaryOutlinedButton(
+                        hasText: true,
+                        title: 'Edit',
+                        onPressed: () {},
                       ),
+                    ],
+                  ),
+                  gapH16,
+                  Text(
+                    'Leslie Flores',
+                    style: Get.textTheme.bodyLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
-                gapH8,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Total',
-                      style: Get.textTheme.displayLarge?.copyWith(
-                        fontWeight: FontWeight.bold,
+                  ),
+                  gapH8,
+                  Column(
+                    children: [
+                      Text(
+                        "2972 Westheimer Rd.Santa Ana, Illinois 85486, United States of America",
+                        style: Get.textTheme.titleLarge?.copyWith(
+                          color: AppColors.neutral600,
+                        ),
                       ),
-                    ),
-                    Text(
-                      '85.98',
-                      style: Get.textTheme.headlineSmall,
-                    ),
-                  ],
-                ),
-                gapH24,
-                const CustomDivider(),
-                PrimaryButton(
-                  buttonColor: AppColors.neutral800,
-                  buttonLabel: 'Pay now',
-                  onPressed: () {},
-                ),
-                gapH16,
-              ],
+                    ],
+                  ),
+                  gapH24,
+                  const CustomDivider(),
+                  gapH24,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Expanded(
+                        flex: 3,
+                        child: PromoCodeTextField(),
+                      ),
+                      gapW8,
+                      Expanded(
+                        child: PrimaryButton(
+                          buttonHeight: 60,
+                          buttonColor: AppColors.neutral800,
+                          buttonLabel: 'Apply',
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                  gapH24,
+                  const CustomDivider(),
+                  gapH24,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Delivery',
+                        style: Get.textTheme.displayLarge?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Free',
+                        style: Get.textTheme.titleLarge?.copyWith(
+                          fontWeight: Fonts.interRegular,
+                        ),
+                      ),
+                    ],
+                  ),
+                  gapH8,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Total',
+                        style: Get.textTheme.displayLarge?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '85.98',
+                        style: Get.textTheme.headlineSmall,
+                      ),
+                    ],
+                  ),
+                  gapH24,
+                  const CustomDivider(),
+                  PrimaryButton(
+                    buttonColor: AppColors.neutral800,
+                    buttonLabel: 'Pay now',
+                    onPressed: () => Get.offAllNamed(AppRoutes.checkoutConfirmationRoute,),
+                  ),
+                  gapH16,
+                ],
+              ),
             ),
           ),
         ),
