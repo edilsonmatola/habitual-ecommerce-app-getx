2aab6c58b11e435ca5f924e5b169099b7b90582d
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-13 15:11:38 +0200

    Finished up Checkout Page

--- lib/src/presentation/checkout_screen/view/checkout_screen.dart
+++ lib/src/presentation/checkout_screen/view/checkout_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:habitual/src/common_widgets/common_widgets_export.dart';
 import 'package:habitual/src/common_widgets/custom_divider.dart';
+import 'package:habitual/src/presentation/checkout_screen/view/components/promo_code_textfield.dart';
 
 import '../../../core/core_export.dart';
 
@@ -67,6 +68,86 @@ class CheckoutScreen extends StatelessWidget {
                       onPressed: () {},
                     ),
                   ],
+                ),
+                gapH16,
+                Text(
+                  'Leslie Flores',
+                  style: Get.textTheme.bodyLarge?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                gapH8,
+                Text(
+                  '''2972 Westheimer Rd. 
+Santa Ana, Illinois 
+85486, United States of America''',
+                  style: Get.textTheme.titleLarge?.copyWith(
+                    color: AppColors.neutral600,
+                  ),
+                ),
+                gapH24,
+                const CustomDivider(),
+                gapH24,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Expanded(
+                      flex: 3,
+                      child: PromoCodeTextField(),
+                    ),
+                    gapW8,
+                    Expanded(
+                      child: PrimaryButton(
+                        buttonHeight: 60,
+                        buttonColor: AppColors.neutral800,
+                        buttonLabel: 'Apply',
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+                gapH24,
+                const CustomDivider(),
+                gapH24,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Delivery',
+                      style: Get.textTheme.displayLarge?.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Free',
+                      style: Get.textTheme.titleLarge?.copyWith(
+                        fontWeight: Fonts.interRegular,
+                      ),
+                    ),
+                  ],
+                ),
+                gapH8,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total',
+                      style: Get.textTheme.displayLarge?.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '85.98',
+                      style: Get.textTheme.headlineSmall,
+                    ),
+                  ],
+                ),
+                gapH24,
+                const CustomDivider(),
+                PrimaryButton(
+                  buttonColor: AppColors.neutral800,
+                  buttonLabel: 'Pay now',
+                  onPressed: () {},
                 ),
                 gapH16,
               ],
