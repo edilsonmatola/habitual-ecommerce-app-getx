2865c9844bed1ac130a323b0151243860755d484
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-13 15:12:08 +0200

    Add Promotion Code Textfield

---
+++ lib/src/presentation/checkout_screen/view/components/promo_code_textfield.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:habitual/src/core/constants/app_colors.dart';
+import 'package:habitual/src/core/constants/app_sizes.dart';
+
+class PromoCodeTextField extends StatelessWidget {
+  const PromoCodeTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        hintText: 'Promo code',
+        hintStyle: Get.textTheme.displaySmall?.copyWith(
+          color: AppColors.neutral400,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.green300,
+          ),
+          borderRadius: BorderRadius.circular(
+            Sizes.p6,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.green300,
+          ),
+          borderRadius: BorderRadius.circular(
+            Sizes.p6,
+          ),
+        ),
+      ),
+    );
+  }
+}
