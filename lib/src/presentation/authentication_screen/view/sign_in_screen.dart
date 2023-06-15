352b7c7cac335db9f8c36f0004e0db898b0a536c
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-15 16:08:32 +0200

    Add Sign In Page

---
+++ lib/src/presentation/authentication_screen/view/sign_in_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            
+          ),
+        ),
+      ),
+    );
+  }
+}
