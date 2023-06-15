52fadf2e05dd0b50e63e82aaeecf7ad383ccac56
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-15 16:06:52 +0200

    Add Registration Page 3

---
+++ lib/src/presentation/registration_screen/view/registration3_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:habitual/src/common_widgets/common_widgets_export.dart';
+import 'package:habitual/src/routes/routes_export.dart';
+
+import '../../../core/core_export.dart';
+import '../widgets/registration_progress_bar.dart';
+
+class Registration3Screen extends StatelessWidget {
+  const Registration3Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.p16,
+          ),
+          child: Column(
+            children: [
+              const RegistrationProgressBar(
+                currentStep: 4,
+                stepName: 'Interests',
+              ),
+              gapH32,
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.p24,
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'What do you want to achieve with Habitual?',
+                      style: Get.textTheme.headlineSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                    gapH40,
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: PrimaryButton(
+                        buttonWidth: 165,
+                        buttonColor: AppColors.neutral800,
+                        buttonLabel: 'Continue',
+                        onPressed: () => Get.toNamed(
+                          AppRoutes.registration4Route,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
