76315883d6291d6520b02d97d5f531d7177ad8e7
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-15 16:00:00 +0200

    Code refactor

--- lib/src/common_widgets/custom_divider.dart
+++ lib/src/common_widgets/custom_divider.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../core/core_export.dart';
 
 class CustomDivider extends StatelessWidget {
-  const CustomDivider({Key? key}) : super(key: key);
+  const CustomDivider({
+    Key? key,
+    required this.hasText,
+    this.text,
+  }) : super(key: key);
+
+  final bool hasText;
+  final String? text;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: [
-        Expanded(
-          child: Divider(
-            thickness: 2,
-            color: AppColors.neutral300,
-          ),
-        ),
-      ],
+      children: hasText
+          ? [
+              Expanded(
+                child: Divider(
+                  thickness: 1,
+                  color: AppColors.neutral300,
+                ),
+              ),
+              gapW16,
+              Text(
+                text!,
+                style: Get.textTheme.bodySmall?.copyWith(
+                  color: AppColors.neutral400,
+                ),
+              ),
+              gapW16,
+              Expanded(
+                child: Divider(
+                  thickness: 1,
+                  color: AppColors.neutral300,
+                ),
+              ),
+            ]
+          : [
+              Expanded(
+                child: Divider(
+                  thickness: 2,
+                  color: AppColors.neutral300,
+                ),
+              ),
+            ],
     );
   }
 }
