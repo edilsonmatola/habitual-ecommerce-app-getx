9ea2a2a9c3ecf911d803dbc2be94e94578b1b8ab
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-21 11:38:32 +0200

    Fixed navigation error

--- lib/src/presentation/authentication_screen/view/sign_up_screen.dart
+++ lib/src/presentation/authentication_screen/view/sign_up_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:habitual/src/common_widgets/common_widgets_export.dart';
 import 'package:habitual/src/common_widgets/custom_divider.dart';
 import 'package:habitual/src/common_widgets/forms/custom_text_field.dart';
-import 'package:habitual/src/presentation/registration_screen/widgets/registration_progress_bar.dart';
 import 'package:habitual/src/routes/routes_export.dart';
 
 import '../../../core/core_export.dart';
@@ -16,125 +16,118 @@ class SignUpScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.p16,
+          padding: const EdgeInsets.fromLTRB(
+            Sizes.p24,
+            Sizes.p24,
+            Sizes.p24,
+            0,
           ),
           child: Column(
             children: [
-              const RegistrationProgressBar(
-                currentStep: 1,
-                stepName: 'Account',
+              // App Logo
+              SvgPicture.asset(
+                AppAssets.appLogoBlack,
               ),
               gapH40,
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.p24,
+              Text(
+                'Create account',
+                style: Get.textTheme.headlineSmall,
+                textAlign: TextAlign.center,
+              ),
+              gapH16,
+              Text(
+                'Find the things that you love!',
+                style: Get.textTheme.bodyMedium?.copyWith(
+                  color: AppColors.neutral600,
+                  fontWeight: Fonts.interRegular,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Create account',
-                      style: Get.textTheme.headlineSmall,
-                      textAlign: TextAlign.center,
-                    ),
-                    gapH16,
-                    Text(
-                      'Find the things that you love!',
-                      style: Get.textTheme.bodyMedium?.copyWith(
-                        color: AppColors.neutral600,
-                        fontWeight: Fonts.interRegular,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    gapH40,
-                    PrimaryOutlinedButton(
-                      hasText: true,
-                      title: 'Sign up with Google',
-                      onPressed: () {},
-                    ),
-                    gapH40,
-                    const CustomDivider(
-                      hasText: true,
-                      text: 'or Sign up with Email',
-                    ),
-                    gapH40,
-                    const CustomTextField(
-                      labelText: 'Full name',
-                      textInputType: TextInputType.text,
-                    ),
-                    gapH16,
-                    const CustomTextField(
-                      labelText: 'Email',
-                      textInputType: TextInputType.emailAddress,
-                    ),
-                    gapH16,
-                    const CustomTextField(
-                      labelText: 'Password',
-                      isSecret: true,
-                    ),
-                    gapH40,
-                    PrimaryButton(
-                      buttonColor: AppColors.neutral800,
-                      buttonLabel: 'Sign Up',
-                      onPressed: () => Get.offAllNamed(
-                        AppRoutes.registration1Route,
-                      ),
-                    ),
-                    gapH24,
-                    Text.rich(
-                      TextSpan(
-                        style: Get.textTheme.bodyMedium,
-                        text: 'By continuing you accept our standard ',
-                        children: const [
-                          WidgetSpan(
-                            child: Text(
-                              'terms and conditions ',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ),
-                          WidgetSpan(
-                            child: Text(
-                              'and our ',
-                              style: TextStyle(),
-                            ),
-                          ),
-                          WidgetSpan(
-                            child: Text(
-                              'privacy policy.',
-                              style: TextStyle(
-                                decoration: TextDecoration.underline,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    gapH24,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Already have an account?',
-                          style: Get.textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+              gapH40,
+              PrimaryOutlinedButton(
+                hasText: true,
+                title: 'Sign up with Google',
+                onPressed: () {},
+              ),
+              gapH40,
+              const CustomDivider(
+                hasText: true,
+                text: 'or Sign up with Email',
+              ),
+              gapH40,
+              const CustomTextField(
+                labelText: 'Full name',
+                textInputType: TextInputType.text,
+              ),
+              gapH16,
+              const CustomTextField(
+                labelText: 'Email',
+                textInputType: TextInputType.emailAddress,
+              ),
+              gapH16,
+              const CustomTextField(
+                labelText: 'Password',
+                isSecret: true,
+              ),
+              gapH40,
+              PrimaryButton(
+                buttonColor: AppColors.neutral800,
+                buttonLabel: 'Sign Up',
+                onPressed: () => Get.offAllNamed(
+                  AppRoutes.registration1Route,
+                ),
+              ),
+              gapH24,
+              Text.rich(
+                TextSpan(
+                  style: Get.textTheme.bodyMedium,
+                  text: 'By continuing you accept our standard ',
+                  children: const [
+                    WidgetSpan(
+                      child: Text(
+                        'terms and conditions ',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
                         ),
-                        PrimaryTextButton(
-                          defaultTextStyle: false,
-                          style: Get.textTheme.bodyMedium?.copyWith(
-                            decoration: TextDecoration.underline,
-                          ),
-                          buttonLabel: 'Log in',
-                          onPressed: () => Get.to(
-                            AppRoutes.signInRoute,
-                          ),
+                      ),
+                    ),
+                    WidgetSpan(
+                      child: Text(
+                        'and our ',
+                        style: TextStyle(),
+                      ),
+                    ),
+                    WidgetSpan(
+                      child: Text(
+                        'privacy policy.',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
                         ),
-                      ],
+                      ),
                     ),
                   ],
                 ),
+                textAlign: TextAlign.center,
+              ),
+              gapH24,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Already have an account?',
+                    style: Get.textTheme.bodyMedium,
+                  ),
+                  PrimaryTextButton(
+                    defaultTextStyle: false,
+                    style: Get.textTheme.bodyMedium?.copyWith(
+                      decoration: TextDecoration.underline,
+                    ),
+                    buttonLabel: 'Log in',
+                    onPressed: () => Get.offAndToNamed(
+                      AppRoutes.signInRoute,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
