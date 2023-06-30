be322257836e8026c0886e926497823d9551e1ee
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-30 22:10:43 +0200

    user category card

---
+++ lib/src/presentation/registration_screen/widgets/user_interest_category_card.dart
@@ -0,0 +1,66 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:habitual/src/common_widgets/common_widgets_export.dart';
+import 'package:habitual/src/core/constants/constants_export.dart';
+
+class UserInterestCategoryCard extends StatelessWidget {
+  const UserInterestCategoryCard({
+    super.key,
+    required this.isSelected,
+    required this.color,
+    required this.category,
+    required this.imageUrl,
+  });
+
+  final bool isSelected;
+  final Color color;
+  final String category;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: Sizes.p16,
+        vertical: Sizes.p24,
+      ),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(
+          Sizes.p10,
+        ),
+      ),
+      child: Column(
+        children: [
+          CachedNetworkImage(
+            placeholder: (_, url) => const Center(
+              child: CircularProgressIndicator.adaptive(),
+            ),
+            imageUrl: imageUrl,
+            fit: BoxFit.cover,
+            alignment: Alignment.center,
+          ),
+          gapH8,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                category,
+                style: Get.textTheme.displayLarge,
+              ),
+              gapW8,
+              PrimaryOutlinedButton(
+                width: 44,
+                height: 36,
+                hasText: true,
+                title: isSelected ? 'Checked' : 'Add',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
