a5f5a376f064726e8d469e644f5664933e6867cb
Edilson Matola <edilsonmatola13@gmail.com>
2023-07-05 13:34:13 +0200

    Add Staggered Card

--- lib/src/presentation/categories_screen/view/categories_screen.dart
+++ lib/src/presentation/categories_screen/view/categories_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:habitual/src/core/core_export.dart';
-import 'package:habitual/src/presentation/categories_screen/widgets/category_card.dart';
+import 'package:habitual/src/presentation/categories_screen/widgets/staggered_category_card.dart';
 
 import '../../../common_widgets/common_widgets_export.dart';
 
@@ -64,6 +64,27 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
     'Toys + Games',
     'Video Games'
   ];
+
+  final popularCategories = [
+    'Tech',
+    'Fashion',
+    'Music',
+    'Reading',
+  ];
+
+  final popularCategoriesColors = [
+    AppColors.purple300,
+    AppColors.blue300,
+    AppColors.red300,
+    AppColors.green300,
+  ];
+
+  final popularCategoriesImage = [
+    'https://media.croma.com/image/upload/v1685969049/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256608_rm160r.png',
+    'https://images.ray-ban.com/is/image/RayBan/8053672845358__002.png?impolicy=RB_RB_FBShare',
+    'https://cdn11.bigcommerce.com/s-h0bqu/images/stencil/1280x1280/products/4398/9412/Fender_Bullet_Stratocaster_HT_Electric_Guitar_Brown_Sunburst__34857.1678809883.png?c=2',
+    'https://bookbins.in/wp-content/uploads/2021/08/The-48-Laws-Of-Power-Robert-Greene-Buy-Online-Bookbins-1.png',
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -103,42 +124,51 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
               child: Column(
                 children: [
                   Text(
-                    'Most Populer',
+                    'Most Popular',
                     style: Get.textTheme.headlineSmall,
                   ),
                   gapH16,
-                  StaggeredGrid.count(
-                    crossAxisCount: 3,
-                    mainAxisSpacing: Sizes.p16,
+                  MasonryGridView.builder(
+                    primary: false,
+                    shrinkWrap: true,
                     crossAxisSpacing: Sizes.p16,
-                    children: [
-                      StaggeredGridTile.count(
-                        crossAxisCellCount: 2,
-                        mainAxisCellCount: 2,
-                        child: CategoryCard(
-                          title: 'Tech',
-                          imageUrl:
-                              'https://media.croma.com/image/upload/v1685969049/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256608_rm160r.png',
-                          color: AppColors.purple300,
-                        ),
+                    mainAxisSpacing: Sizes.p16,
+                    itemCount: popularCategories.length,
+                    gridDelegate:
+                        const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemBuilder: (_, index) => StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: index.isEven ? 3 : 2,
+                      child: StaggeredCard(
+                        color: popularCategoriesColors[index],
+                        categoryName: popularCategories[index],
+                        imageUrl: popularCategoriesImage[index],
+                        onTap: () {},
                       ),
-                      StaggeredGridTile.count(
-                        crossAxisCellCount: 2,
-                        mainAxisCellCount: 2,
-                        child: Container(color: Colors.blue),
-                      ),
-                      StaggeredGridTile.count(
-                        crossAxisCellCount: 2,
-                        mainAxisCellCount: 2,
-                        child: Container(color: Colors.blue),
-                      ),
-                      StaggeredGridTile.count(
-                        crossAxisCellCount: 2,
-                        mainAxisCellCount: 2,
-                        child: Container(color: Colors.blue),
-                      ),
-                    ],
+                    ),
                   ),
+                  // StaggeredGrid.count(
+                  //   crossAxisCount: 3,
+                  //   mainAxisSpacing: Sizes.p16,
+                  //   crossAxisSpacing: Sizes.p16,
+                  //   children: List.generate(
+                  //     6,
+                  //     (index) => StaggeredGridTile.count(
+                  //       crossAxisCellCount: 4,
+                  //       mainAxisCellCount: 4,
+                  //       child: ListView.builder(
+                  //         itemBuilder: (context, index) => StaggeredCard(
+                  //           color: AppColors.purple300,
+                  //           categoryName: 'Tech',
+                  //           imageUrl:
+                  //               'https://media.croma.com/image/upload/v1685969049/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256608_rm160r.png',
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
                   gapH24,
                   Text(
                     'All Categories',
