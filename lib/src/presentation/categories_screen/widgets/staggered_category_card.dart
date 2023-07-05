24007dd5b74fd3abd6a73657740ac723f1001b36
Edilson Matola <edilsonmatola13@gmail.com>
2023-07-05 13:35:13 +0200

    Finished Up Staggered Category

--- lib/src/presentation/categories_screen/widgets/staggered_category_card.dart
+++ lib/src/presentation/categories_screen/widgets/staggered_category_card.dart
@@ -1,37 +1,39 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:habitual/src/core/constants/app_colors.dart';
+import 'package:get/get.dart';
+import 'package:habitual/src/common_widgets/common_widgets_export.dart';
+import 'package:habitual/src/core/constants/app_fonts.dart';
 import 'package:habitual/src/core/constants/app_sizes.dart';
 
 class CategoryCard extends StatelessWidget {
-  final Color begin;
-  final Color end;
+  final Color color;
   final String categoryName;
   final String imageUrl;
+  final VoidCallback? onTap;
 
   CategoryCard({
     super.key,
     required this.controller,
-    required this.begin,
-    required this.end,
+    required this.color,
     required this.categoryName,
     required this.imageUrl,
-  })  : height = Tween<double>(begin: 150, end: 250.0).animate(
+    this.onTap,
+  })  : height = Tween<double>(begin: 270, end: 150).animate(
           CurvedAnimation(
             parent: controller,
             curve: const Interval(
-              0.0,
-              0.300,
+              0,
+              .300,
               curve: Curves.ease,
             ),
           ),
         ),
-        itemHeight = Tween<double>(begin: 0, end: 150.0).animate(
+        itemHeight = Tween<double>(begin: 150, end: 0).animate(
           CurvedAnimation(
             parent: controller,
             curve: const Interval(
-              0.0,
-              0.300,
+              0,
+              .300,
               curve: Curves.ease,
             ),
           ),
@@ -41,107 +43,79 @@ class CategoryCard extends StatelessWidget {
   final Animation<double> height;
   final Animation<double> itemHeight;
 
-  // This function is called each time the controller "ticks" a new frame.
-  // When it runs, all of the animation's values will have been
-  // updated to reflect the controller's current value.
-  Widget _buildAnimation(BuildContext context, Widget? child) {
-    return Container(
-      height: height.value,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-            colors: [begin, end],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(
-            Sizes.p10,
-          ),
-        ),
-      ),
-      padding: const EdgeInsets.all(Sizes.p16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Align(
-              alignment: const Alignment(-1, 0),
-              child: Text(
-                categoryName,
-                style: TextStyle(
-                  fontSize: 22,
-                  color: AppColors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              )),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-//        mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                height: itemHeight.value,
-                child: CachedNetworkImage(
-                  imageUrl: imageUrl,
-                  fit: BoxFit.contain,
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: const BorderRadius.all(
-                    Radius.circular(
-                      Sizes.p24,
-                    ),
-                  ),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.p16,
-                  vertical: Sizes.p8,
-                ),
-                child: Text(
-                  'View more',
-                  style: TextStyle(
-                    color: end,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      builder: _buildAnimation,
       animation: controller,
+      builder: (_, child) => Container(
+        height: height.value,
+        width: Sizes.deviceWidth * .5,
+        padding: const EdgeInsetsDirectional.all(
+          Sizes.p16,
+        ),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(
+              Sizes.p10,
+            ),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              categoryName,
+              style: Get.textTheme.displayLarge?.copyWith(
+                fontWeight: Fonts.interSemiBold,
+              ),
+            ),
+            gapH8,
+            Center(
+              child: CachedNetworkImage(
+                alignment: Alignment.center,
+                placeholder: (_, url) => const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                ),
+                imageUrl: imageUrl,
+                height: itemHeight.value,
+                fit: BoxFit.contain,
+              ),
+            ),
+            gapH8,
+            Align(
+              alignment: Alignment.centerRight,
+              child: PrimaryOutlinedButton(
+                hasText: false,
+                onPressed: onTap,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class StaggeredCardCard extends StatefulWidget {
-  final Color begin;
-  final Color end;
+class StaggeredCard extends StatefulWidget {
+  final Color color;
   final String categoryName;
-  final String assetPath;
+  final String imageUrl;
+  final VoidCallback? onTap;
 
-  const StaggeredCardCard({
+  const StaggeredCard({
     super.key,
-    required this.begin,
-    required this.end,
+    required this.color,
     required this.categoryName,
-    required this.assetPath,
+    required this.imageUrl,
+    this.onTap,
   });
 
   @override
-  StaggeredCardCardState createState() => StaggeredCardCardState();
+  StaggeredCardState createState() => StaggeredCardState();
 }
 
-class StaggeredCardCardState extends State<StaggeredCardCard>
+class StaggeredCardState extends State<StaggeredCard>
     with TickerProviderStateMixin {
   late AnimationController _controller;
   bool isActive = false;
@@ -151,7 +125,9 @@ class StaggeredCardCardState extends State<StaggeredCardCard>
     super.initState();
 
     _controller = AnimationController(
-        duration: const Duration(milliseconds: 300), vsync: this);
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
   }
 
   Future<void> _playAnimation() async {
@@ -172,7 +148,6 @@ class StaggeredCardCardState extends State<StaggeredCardCard>
 
   @override
   Widget build(BuildContext context) {
-    var timeDilation = 10.0; // 1.0 is normal animation speed.
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
       onTap: () {
@@ -187,9 +162,9 @@ class StaggeredCardCardState extends State<StaggeredCardCard>
       child: CategoryCard(
         controller: _controller.view,
         categoryName: widget.categoryName,
-        begin: widget.begin,
-        end: widget.end,
-        imageUrl: widget.assetPath,
+        color: widget.color,
+        imageUrl: widget.imageUrl,
+        onTap: widget.onTap,
       ),
     );
   }
