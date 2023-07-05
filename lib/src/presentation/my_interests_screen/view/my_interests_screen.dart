418506f099a3ddfb23c5177d3c0c2686c21b2473
Edilson Matola <edilsonmatola13@gmail.com>
2023-07-05 14:25:20 +0200

    Code refactor + Add TODO

--- lib/src/presentation/my_interests_screen/view/my_interests_screen.dart
+++ lib/src/presentation/my_interests_screen/view/my_interests_screen.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 
 import '../../../common_widgets/common_widgets_export.dart';
 import '../../../core/core_export.dart';
-import '../../../routes/app_pages.dart';
+import '../../categories_screen/widgets/staggered_category_card.dart';
 
-class MyInterestsScreen extends StatelessWidget {
+class MyInterestsScreen extends StatefulWidget {
   const MyInterestsScreen({super.key});
+
+  @override
+  State<MyInterestsScreen> createState() => _MyInterestsScreenState();
+}
+
+class _MyInterestsScreenState extends State<MyInterestsScreen> {
+  final popularCategories = [
+    'Tech',
+    'Fashion',
+    'Music',
+    'Reading',
+  ];
+
+  final popularCategoriesColors = [
+    AppColors.purple300,
+    AppColors.blue300,
+    AppColors.red300,
+    AppColors.green300,
+  ];
+
+  final popularCategoriesImage = [
+    'https://media.croma.com/image/upload/v1685969049/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256608_rm160r.png',
+    'https://images.ray-ban.com/is/image/RayBan/8053672845358__002.png?impolicy=RB_RB_FBShare',
+    'https://cdn11.bigcommerce.com/s-h0bqu/images/stencil/1280x1280/products/4398/9412/Fender_Bullet_Stratocaster_HT_Electric_Guitar_Brown_Sunburst__34857.1678809883.png?c=2',
+    'https://bookbins.in/wp-content/uploads/2021/08/The-48-Laws-Of-Power-Robert-Greene-Buy-Online-Bookbins-1.png',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +42,12 @@ class MyInterestsScreen extends StatelessWidget {
         body: NestedScrollView(
           headerSliverBuilder: (context, innerBoxIsScrolled) => [
             SliverAppBar(
-              centerTitle: false,
               title: Padding(
                 padding: const EdgeInsets.only(
                   left: Sizes.p8,
                 ),
                 child: Text(
-                  AppTitles.profileTitle,
+                  'My Interests',
                   style: Get.textTheme.headlineSmall,
                 ),
               ),
@@ -31,10 +57,8 @@ class MyInterestsScreen extends StatelessWidget {
                     right: Sizes.p24,
                   ),
                   child: PrimaryIconButton(
-                    icon: AppIcons.logoutIcon,
-                    onPressed: () => Get.offAllNamed(
-                      AppRoutes.signInRoute,
-                    ),
+                    icon: AppIcons.shoppingCartIcon,
+                    onPressed: () {},
                   ),
                 ),
               ],
@@ -47,6 +71,7 @@ class MyInterestsScreen extends StatelessWidget {
                 Sizes.p24,
               ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -63,7 +88,30 @@ class MyInterestsScreen extends StatelessWidget {
                     ],
                   ),
                   gapH16,
-                  // TODO: Staggered Grid View
+                  MasonryGridView.builder(
+                    primary: false,
+                    shrinkWrap: true,
+                    crossAxisSpacing: Sizes.p16,
+                    mainAxisSpacing: Sizes.p16,
+                    itemCount: popularCategories.length,
+                    gridDelegate:
+                        const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemBuilder: (_, index) => StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: index.isEven ? 3 : 2,
+                      child: StaggeredCard(
+                        color: popularCategoriesColors[index],
+                        categoryName: popularCategories[index],
+                        imageUrl: popularCategoriesImage[index],
+                        onTap: () {},
+                      ),
+                    ),
+                  ),
+                  gapH24,
+                  // TODO: Trending in Reading
+                  // TODO: Trending in Gaming
                 ],
               ),
             ),
