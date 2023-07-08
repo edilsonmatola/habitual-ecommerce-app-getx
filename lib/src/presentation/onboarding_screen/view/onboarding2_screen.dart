2c1312c7e283b51dfbf12e498bc1e7a9de9f881e
Edilson Matola <edilsonmatola13@gmail.com>
2023-07-08 10:19:54 +0200

    Code refactor

--- lib/src/presentation/onboarding_screen/view/onboarding2_screen.dart
+++ lib/src/presentation/onboarding_screen/view/onboarding2_screen.dart
@@ -2,60 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
-import '../../../common_widgets/common_widgets_export.dart';
 import '../../../core/core_export.dart';
-import '../../../routes/routes_export.dart';
 import '../widgets/header_section.dart';
 
 class OnBoarding2Screen extends StatelessWidget {
-  const OnBoarding2Screen({Key? key}) : super(key: key);
+  const OnBoarding2Screen({Key? key, this.onClick}) : super(key: key);
+
+  final VoidCallback? onClick;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          padding: onboardingScreenPadding,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              HeaderSection(
-                onPressed: () => Get.toNamed(
-                  AppRoutes.onBoarding3Route,
-                ),
-              ),
-              gapH16,
-              SvgPicture.asset(
-                AppAssets.productsImage,
-                width: 342,
-                height: 342,
-              ),
-              Text(
-                AppTitles.onboarding2Title,
-                style: Get.textTheme.headlineMedium,
-                textAlign: TextAlign.center,
-              ),
-              gapH16,
-              Text(
-                AppTitles.onboarding2Description,
-                style: Get.textTheme.displaySmall?.copyWith(
-                  color: AppColors.neutral600,
-                  fontWeight: Fonts.interRegular,
-                  height: 1.5,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              gapH32,
-              PrimaryButton(
-                buttonLabel: 'Next',
-                buttonColor: AppColors.neutral800,
-                onPressed: () => Get.toNamed(
-                  AppRoutes.onBoarding3Route,
-                ),
-              ),
-            ],
+    return SingleChildScrollView(
+      padding: onboardingScreenPadding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          HeaderSection(
+            onPressed: onClick,
           ),
-        ),
+          gapH16,
+          SvgPicture.asset(
+            AppAssets.productsImage,
+            width: 320,
+            height: 320,
+          ),
+          Text(
+            AppTitles.onboarding2Title,
+            style: Get.textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          gapH16,
+          Text(
+            AppTitles.onboarding2Description,
+            style: Get.textTheme.displaySmall?.copyWith(
+              color: AppColors.neutral600,
+              fontWeight: Fonts.interRegular,
+              height: 1.5,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          // gapH32,
+          // PrimaryButton(
+          //   buttonLabel: 'Next',
+          //   buttonColor: AppColors.neutral800,
+          //   onPressed: () => Get.toNamed(
+          //     AppRoutes.onBoarding3Route,
+          //   ),
+          // ),
+        ],
       ),
     );
   }
--- lib/src/presentation/onboarding_screen/view/onboarding3_screen.dart
+++ lib/src/presentation/onboarding_screen/view/onboarding3_screen.dart
@@ -2,62 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
-import '../../../common_widgets/common_widgets_export.dart';
 import '../../../core/core_export.dart';
-import '../../../routes/routes_export.dart';
 import '../widgets/header_section.dart';
 
 class OnBoarding3Screen extends StatelessWidget {
-  const OnBoarding3Screen({Key? key}) : super(key: key);
+  const OnBoarding3Screen({Key? key, this.onClick}) : super(key: key);
+
+  final VoidCallback? onClick;
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SingleChildScrollView(
-          padding: onboardingScreenPadding,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              HeaderSection(
-                buttonLabel: 'Exit',
-                onPressed: () => Get.offAllNamed(
-                  AppRoutes.baseRoute,
-                ),
-              ),
-              gapH16,
-              SvgPicture.asset(
-                AppAssets.mailboxImage,
-                width: 342,
-                height: 342,
-              ),
-              Text(
-                AppTitles.onboarding3Title,
-                style: Get.textTheme.headlineMedium,
-                textAlign: TextAlign.center,
-              ),
-              gapH16,
-              Text(
-                AppTitles.onboarding3Description,
-                style: Get.textTheme.displaySmall?.copyWith(
-                  color: AppColors.neutral600,
-                  fontWeight: Fonts.interRegular,
-                  height: 1.5,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              gapH32,
-              PrimaryButton(
-                buttonLabel: 'Sign me up!',
-                buttonColor: AppColors.yellow300,
-                labelColor: AppColors.neutral800,
-                onPressed: () => Get.toNamed(
-                  AppRoutes.registration1Route,
-                ),
-              ),
-            ],
+    return SingleChildScrollView(
+      padding: onboardingScreenPadding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          HeaderSection(
+            buttonLabel: 'Exit',
+            onPressed: onClick,
           ),
-        ),
+          gapH16,
+          SvgPicture.asset(
+            AppAssets.mailboxImage,
+            width: 320,
+            height: 320,
+          ),
+          Text(
+            AppTitles.onboarding3Title,
+            style: Get.textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          gapH16,
+          Text(
+            AppTitles.onboarding3Description,
+            style: Get.textTheme.displaySmall?.copyWith(
+              color: AppColors.neutral600,
+              fontWeight: Fonts.interRegular,
+              height: 1.5,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          // gapH32,
+          // PrimaryButton(
+          //   buttonLabel: 'Sign me up!',
+          //   buttonColor: AppColors.yellow300,
+          //   labelColor: AppColors.neutral800,
+          //   onPressed: () => Get.toNamed(
+          //     AppRoutes.registration1Route,
+          //   ),
+          // ),
+        ],
       ),
     );
   }
