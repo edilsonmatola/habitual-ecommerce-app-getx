e7373b583366b64a53bbbe4ef6f90a79d6896cce
Edilson Matola <edilsonmatola13@gmail.com>
2023-07-11 11:20:21 +0200

    Add bool attribute

--- lib/src/presentation/registration_screen/widgets/user_goal_card.dart
+++ lib/src/presentation/registration_screen/widgets/user_goal_card.dart
@@ -6,18 +6,18 @@ class UserGoalCard extends StatelessWidget {
   const UserGoalCard({
     super.key,
     required this.text,
-    // required this.isSelected,
+    required this.isSelected,
     this.onTap,
-    required this.cardColor,
-    required this.borderColor,
-    required this.textColor,
+    // required this.cardColor,
+    // required this.borderColor,
+    // required this.textColor,
   });
 
   final String text;
-  final Color cardColor;
-  final Color borderColor;
-  final Color textColor;
-  // final bool isSelected;
+  // final Color cardColor;
+  // final Color borderColor;
+  // final Color textColor;
+  final bool isSelected;
   final VoidCallback? onTap;
 
   @override
@@ -32,19 +32,19 @@ class UserGoalCard extends StatelessWidget {
           vertical: Sizes.p16,
         ),
         decoration: BoxDecoration(
-          // color: isSelected ? Colors.transparent : AppColors.neutral800,
-          color: cardColor,
+          color: isSelected ? Colors.transparent : AppColors.neutral800,
+          // color: cardColor,
           border: Border.all(
-            color: borderColor,
-            // color: isSelected ? AppColors.neutral800 : AppColors.neutral300,
+            // color: borderColor,
+            color: isSelected ? AppColors.neutral800 : AppColors.neutral300,
           ),
           borderRadius: BorderRadius.circular(100),
         ),
         child: Text(
           text,
           style: Get.textTheme.bodyMedium?.copyWith(
-            color: textColor,
-            // color: isSelected ? AppColors.neutral800 : AppColors.white,
+            // color: textColor,
+            color: isSelected ? AppColors.neutral800 : AppColors.white,
             fontWeight: Fonts.interRegular,
           ),
           textAlign: TextAlign.center,
