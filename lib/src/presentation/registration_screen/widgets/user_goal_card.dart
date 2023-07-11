0b32fb655789171b35f8a07cd12e44102567360f
Edilson Matola <edilsonmatola13@gmail.com>
2023-07-11 10:53:59 +0200

    Code refactor

--- lib/src/presentation/registration_screen/widgets/user_goal_card.dart
+++ lib/src/presentation/registration_screen/widgets/user_goal_card.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:habitual/src/core/constants/app_colors.dart';
-import 'package:habitual/src/core/constants/app_fonts.dart';
-import 'package:habitual/src/core/constants/app_sizes.dart';
+import 'package:habitual/src/core/core_export.dart';
 
 class UserGoalCard extends StatelessWidget {
   const UserGoalCard({
     super.key,
     required this.text,
-    required this.isSelected,
+    // required this.isSelected,
     this.onTap,
+    required this.cardColor,
+    required this.borderColor,
+    required this.textColor,
   });
 
   final String text;
-  final bool isSelected;
+  final Color cardColor;
+  final Color borderColor;
+  final Color textColor;
+  // final bool isSelected;
   final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      highlightColor: AppColors.neutral200.withOpacity(.50),
+      borderRadius: BorderRadius.circular(100),
       onTap: onTap,
       child: Container(
         width: Sizes.deviceWidth * .90,
@@ -26,16 +32,19 @@ class UserGoalCard extends StatelessWidget {
           vertical: Sizes.p16,
         ),
         decoration: BoxDecoration(
+          // color: isSelected ? Colors.transparent : AppColors.neutral800,
+          color: cardColor,
           border: Border.all(
-            color: isSelected ? AppColors.neutral800 : AppColors.neutral300,
+            color: borderColor,
+            // color: isSelected ? AppColors.neutral800 : AppColors.neutral300,
           ),
-          color: isSelected ? Colors.transparent : AppColors.neutral800,
           borderRadius: BorderRadius.circular(100),
         ),
         child: Text(
           text,
           style: Get.textTheme.bodyMedium?.copyWith(
-            color: isSelected ? AppColors.neutral800 : AppColors.white,
+            color: textColor,
+            // color: isSelected ? AppColors.neutral800 : AppColors.white,
             fontWeight: Fonts.interRegular,
           ),
           textAlign: TextAlign.center,
