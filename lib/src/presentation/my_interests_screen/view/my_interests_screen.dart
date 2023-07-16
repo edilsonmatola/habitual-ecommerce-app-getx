d3b53cee3c07b39b2f29b9b3a71f1222a1e647c3
Edilson Matola <edilsonmatola13@gmail.com>
2023-07-16 21:28:04 +0200

    Add gaming Images + Product card

--- lib/src/presentation/my_interests_screen/view/my_interests_screen.dart
+++ lib/src/presentation/my_interests_screen/view/my_interests_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
+import 'package:habitual/src/presentation/home_screen/widgets/deals_card.dart';
 
 import '../../../common_widgets/common_widgets_export.dart';
 import '../../../core/core_export.dart';
@@ -37,16 +38,18 @@ class _MyInterestsScreenState extends State<MyInterestsScreen> {
 
   final trendingInGamingImages = [
     'https://tech4u.co.mz/wp-content/uploads/2023/01/cq5dam.web_.1280.1280.png',
-    'https://images.csmonitor.com/csm/2014/06/hobbit.png?alias=standard_900x600nc',
+    'https://icegames.co/image/cache/catalog/1212121219/dualsense-ps5-controller-midnight-black-accessory-front-550x550.png',
+    // 'https://images.csmonitor.com/csm/2014/06/hobbit.png?alias=standard_900x600nc',
     'https://multimedia.bbycastatic.ca/multimedia/products/1500x1500/171/17145/17145330_8.png',
-    'https://media2.sport-bittl.com/images/product_images/original_images/27826167676a_Birkenstock_Arizona_Schuh_He_schwarz.png',
+    'https://res-4.cloudinary.com/grover/image/upload/v1630929070/vyqf9rdpila7hephrw75.png',
+    // 'https://media2.sport-bittl.com/images/product_images/original_images/27826167676a_Birkenstock_Arizona_Schuh_He_schwarz.png',
   ];
 
   final trendingInReadingImages = [
-    'https://tech4u.co.mz/wp-content/uploads/2023/01/cq5dam.web_.1280.1280.png',
-    'https://images.csmonitor.com/csm/2014/06/hobbit.png?alias=standard_900x600nc',
-    'https://multimedia.bbycastatic.ca/multimedia/products/1500x1500/171/17145/17145330_8.png',
-    'https://media2.sport-bittl.com/images/product_images/original_images/27826167676a_Birkenstock_Arizona_Schuh_He_schwarz.png',
+    'https://bookbins.in/wp-content/uploads/2023/01/Cant-Hurt-Me-David-Goggins-Buy-Online-Bookbins-1.png',
+    'https://bookbins.in/wp-content/uploads/2022/02/How-To-Stop-Worrying-And-Start-Living-Dale-Carnegie-Buy-Online-Bookbins-1.png',
+    'https://assets.target.com.au/transform/025d6e4b-5f99-4ac9-ab9a-ee6b4113639e/65141670_IMG_001?io=transform:extend,width:700,height:800&output=jpeg&quality=80',
+    'https://bookbins.in/wp-content/uploads/2021/11/Who-Moved-My-Cheese-Dr.Spencer-Johnson-Buy-Online-Bookbins-1.png',
   ];
 
   @override
@@ -137,6 +140,21 @@ class _MyInterestsScreenState extends State<MyInterestsScreen> {
                         buttonLabel: 'View all',
                         onPressed: () {},
                       )
+                    ],
+                  ),
+                  StaggeredGrid.count(
+                    crossAxisSpacing: Sizes.p16,
+                    mainAxisSpacing: Sizes.p16,
+                    crossAxisCount: 2,
+                    children: [
+                      ...List.generate(
+                        trendingInGamingImages.length,
+                        (index) => DealsCard(
+                          imageUrl: trendingInGamingImages[index],
+                          onCardTap: () {},
+                          onLikeTap: () {},
+                        ),
+                      ),
                     ],
                   ),
                   gapH24,
