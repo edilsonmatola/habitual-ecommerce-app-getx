4031d0eef9f100cfee33d0ee63d46e9e36aae463
Edilson Matola <edilsonmatola13@gmail.com>
2023-07-19 11:05:48 +0200

    Add visibility to check UserLoggedIn

--- lib/src/presentation/profile_screen/profile_tab_screen.dart
+++ lib/src/presentation/profile_screen/profile_tab_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 import 'package:habitual/src/presentation/profile_screen/widgets/account_card.dart';
 import 'package:habitual/src/presentation/profile_screen/widgets/profile_biography_field.dart';
@@ -6,6 +7,7 @@ import 'package:habitual/src/routes/app_pages.dart';
 
 import '../../common_widgets/common_widgets_export.dart';
 import '../../core/core_export.dart';
+import '../categories_screen/widgets/staggered_category_card.dart';
 
 class ProfileTabScreen extends StatefulWidget {
   const ProfileTabScreen({super.key});
@@ -15,6 +17,27 @@ class ProfileTabScreen extends StatefulWidget {
 }
 
 class _ProfileTabScreenState extends State<ProfileTabScreen> {
+  final popularCategories = [
+    'Tech',
+    'Fashion',
+    'Music',
+    'Reading',
+  ];
+
+  final popularCategoriesColors = [
+    AppColors.purple300,
+    AppColors.blue300,
+    AppColors.red300,
+    AppColors.green300,
+  ];
+
+  final popularCategoriesImage = [
+    'https://media.croma.com/image/upload/v1685969049/Croma%20Assets/Computers%20Peripherals/Laptop/Images/256608_rm160r.png',
+    'https://images.ray-ban.com/is/image/RayBan/8053672845358__002.png?impolicy=RB_RB_FBShare',
+    'https://cdn11.bigcommerce.com/s-h0bqu/images/stencil/1280x1280/products/4398/9412/Fender_Bullet_Stratocaster_HT_Electric_Guitar_Brown_Sunburst__34857.1678809883.png?c=2',
+    'https://bookbins.in/wp-content/uploads/2021/08/The-48-Laws-Of-Power-Robert-Greene-Buy-Online-Bookbins-1.png',
+  ];
+
   final accountSettings = [
     'Orders',
     'Payment Methods',
@@ -24,6 +47,7 @@ class _ProfileTabScreenState extends State<ProfileTabScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoggedIn = true.obs;
     return Scaffold(
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
@@ -69,25 +93,71 @@ class _ProfileTabScreenState extends State<ProfileTabScreen> {
                   editFunction: () {},
                 ),
                 gapH24,
-                EmptyStateCard(
-                  hasDescription: true,
-                  cardImage: AppAssets.profileEmpty,
-                  cardTitle: 'What interests you!',
-                  cardDescription:
-                      "You don't have any interests listed. Tell us what you love the most and we'll recommend relevant products to you.",
-                  cardColor: AppColors.red300,
-                  buttonText: '+ Add my interests',
-                  buttonPressed: () => Get.toNamed(
-                    AppRoutes.myInterestsRoute,
+                Visibility(
+                  visible: isLoggedIn.value,
+                  replacement: EmptyStateCard(
+                    hasDescription: true,
+                    cardImage: AppAssets.profileEmpty,
+                    cardTitle: 'What interests you!',
+                    cardDescription:
+                        "You don't have any interests listed. Tell us what you love the most and we'll recommend relevant products to you.",
+                    cardColor: AppColors.red300,
+                    buttonText: '+ Add my interests',
+                    buttonPressed: () => Get.toNamed(
+                      AppRoutes.myInterestsRoute,
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Interests',
+                            style: Get.textTheme.displayLarge,
+                          ),
+                          PrimaryTextButton(
+                            buttonLabel: 'View all',
+                            onPressed: () => Get.toNamed(
+                              AppRoutes.myInterestsRoute,
+                            ),
+                          ),
+                        ],
+                      ),
+                      gapH16,
+                      MasonryGridView.builder(
+                        primary: false,
+                        shrinkWrap: true,
+                        crossAxisSpacing: Sizes.p16,
+                        mainAxisSpacing: Sizes.p16,
+                        itemCount: popularCategories.length,
+                        gridDelegate:
+                            const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                        ),
+                        itemBuilder: (_, index) => StaggeredGridTile.count(
+                          crossAxisCellCount: 2,
+                          mainAxisCellCount: index.isEven ? 3 : 2,
+                          child: StaggeredCard(
+                            color: popularCategoriesColors[index],
+                            categoryName: popularCategories[index],
+                            imageUrl: popularCategoriesImage[index],
+                            onTap: () {},
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+                gapH24,
                 Row(
                   children: [
                     Expanded(
                       flex: 2,
                       child: Text(
                         'Account',
-                        style: Theme.of(context).textTheme.headlineSmall,
+                        style: Theme.of(context).textTheme.displayLarge,
                       ),
                     ),
                     PrimaryTextButton(
